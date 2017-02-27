7ccc373050c676abcf0445de31243195f07f3732
Patrick Geissel <geissel@enews.nrl.navy.mil>
2017-02-27 16:33:51 -0500

    DEV: SDK: LoadEarthFile example shows elevation under mouse cursor, to test SIM-7207, review 9652

--- Examples/LoadEarthFile/LoadEarthFile.cpp
+++ Examples/LoadEarthFile/LoadEarthFile.cpp
@@ -32,6 +32,8 @@
 #include "simVis/InsetViewEventHandler.h"
 #include "simVis/Platform.h"
 #include "simUtil/DbConfigurationFile.h"
+#include "simUtil/MouseDispatcher.h"
+#include "simUtil/MousePositionManipulator.h"
 #include "simUtil/PlatformSimulator.h"
 #include "simUtil/ExampleResources.h"
 
@@ -53,17 +55,19 @@ static std::string s_title =
 static std::string s_help =
   "1 : load next earth file\n"
   "2 : load next earth file (map only)\n"
+  "e : toggle show lat/lon/elevation\n"
   "i : toggle add-inset mouse mode\n"
   "r : remove all insets\n";
 
 
-static Control* createHelp()
+static Control* createHelp(LabelControl* elevationLabel)
 {
   VBox* vbox = new VBox();
   vbox->setPadding(10);
   vbox->setBackColor(0, 0, 0, 0.6);
   vbox->addControl(new LabelControl(s_title, 20, osg::Vec4f(1, 1, 0, 1)));
   vbox->addControl(new LabelControl(s_help, 14, osg::Vec4f(.8, .8, .8, 1)));
+  vbox->addControl(elevationLabel);
   return vbox;
 }
 
@@ -87,13 +91,59 @@ void earthFileLoader::loadEarthFile(const std::string& earthFile,
 }
 
 
+// A mouse position listener to update the elevation label with the current lat/lon/elevation value under the mouse
+class LatLonElevationListener : public simUtil::MousePositionManipulator::Listener
+{
+public:
+  explicit LatLonElevationListener(LabelControl* elevationLabel)
+    : elevationLabel_(elevationLabel),
+      showLatLonElevation_(false),
+      lastElevation_(0.0)
+  {
+  }
+  virtual void mouseOverLatLon(double lat, double lon, double elev)
+  {
+    if (!showLatLonElevation_)
+      return;
+    std::ostringstream os;
+    os << "Lat: " << lat << ", Lon: " << lon;
+    os << ", Elevation: ";
+    if (elev == simUtil::MousePositionManipulator::INVALID_POSITION_VALUE)
+      os << "INVALID";
+    else
+      os << elev;
+    elevationLabel_->setText(os.str());
+  }
+
+  void showLatLonElevation(bool show)
+  {
+    if (show == showLatLonElevation_)
+      return;
+    showLatLonElevation_ = show;
+
+    if (!show)
+      elevationLabel_->setText("");
+  }
+
+private:
+  LabelControl* elevationLabel_;
+  bool showLatLonElevation_;
+  double lastElevation_;
+};
+
 /// An event handler to assist in testing the InsetViewManager / Load Earth functionality.
 struct MenuHandler : public osgGA::GUIEventHandler
 {
-  MenuHandler(simVis::Viewer* viewer, simVis::InsetViewEventHandler* handler)
+  MenuHandler(simVis::Viewer* viewer, simVis::InsetViewEventHandler* handler, LabelControl* elevationLabel, simUtil::MouseDispatcher* mouseDispatcher)
    : viewer_(viewer),
-     handler_(handler)
+     handler_(handler),
+     latLonElevListener_(NULL),
+     showLatLonElevation_(false)
   {
+    latLonElevListener_ = new LatLonElevationListener(elevationLabel);
+    mouseDispatcher_ = mouseDispatcher;
+    mouseDispatcher_->setViewManager(NULL);
+    setUpMouseManip_(viewer_);
   }
 
   bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
@@ -122,14 +172,29 @@ struct MenuHandler : public osgGA::GUIEventHandler
 
       case 'l': // LOAD EARTH FILE
       case '1':
+        mouseManip_->removeListener(latLonElevListener_);
         earthFileIndex = (earthFileIndex + 1) % earthFiles.size();
         earthFileLoader::loadEarthFile(earthFiles[earthFileIndex], viewer_, false);
+        setUpMouseManip_(viewer_);
         handled = true;
         break;
 
       case '2': // LOAD EARTH FILE, MAP ONLY
+        mouseManip_->removeListener(latLonElevListener_);
         earthFileIndex = (earthFileIndex + 1) % earthFiles.size();
         earthFileLoader::loadEarthFile(earthFiles[earthFileIndex], viewer_, true);
+        setUpMouseManip_(viewer_);
+        handled = true;
+        break;
+
+      case 'e': // TOGGLE SHOW LAT/LON/ELEVATION
+        // always remove listener
+        mouseManip_->removeListener(latLonElevListener_);
+        showLatLonElevation_ = !showLatLonElevation_;
+        // if showing elevation, add the elevation mouse listener
+        if (showLatLonElevation_)
+          mouseManip_->addListener(latLonElevListener_, true);
+        latLonElevListener_->showLatLonElevation(showLatLonElevation_);
         handled = true;
         break;
       }
@@ -138,13 +203,31 @@ struct MenuHandler : public osgGA::GUIEventHandler
   }
 
 private:
+
+  // create a new mouse position manipulator for the specified viewer, set the viewer as view manager in the mouse dispatcher
+  void setUpMouseManip_(simVis::Viewer* viewer)
+  {
+    mouseManip_.reset(new simUtil::MousePositionManipulator(viewer->getSceneManager()->getMapNode(), viewer->getSceneManager()->getOrCreateAttachPoint("Map Callbacks")));
+    mouseManip_->setTerrainResolution(0.0001);
+    mouseDispatcher_->setViewManager(viewer);
+    mouseDispatcher_->addManipulator(0, mouseManip_);
+    if (showLatLonElevation_)
+      mouseManip_->addListener(latLonElevListener_, true);
+  }
+
   simVis::Viewer* viewer_;
   simVis::InsetViewEventHandler* handler_;
+  LatLonElevationListener* latLonElevListener_;
+  bool showLatLonElevation_;
+  simUtil::MouseDispatcher* mouseDispatcher_;
+  std::shared_ptr<simUtil::MousePositionManipulator> mouseManip_;
 };
 
 
 int main(int argc, char** argv)
 {
+  bool useRex = true;
+
   // Set up the scene:
   simCore::checkVersionThrow();
   simExamples::configureSearchPaths();
@@ -152,7 +235,8 @@ int main(int argc, char** argv)
   if (argc < 3)
   {
     std::cerr << "USAGE: pass in earth files on command line: \n"
-      << " --earthFiles <file1> <file2> ...\n";
+      << " --earthFiles <file1> <file2> ...\n"
+      << " [--useMp]\n";
     return 0;
   }
 
@@ -170,6 +254,8 @@ int main(int argc, char** argv)
       }
       index = newIndex;
     }
+    if (arg == "--useMp")
+      useRex = false;
   }
 
   if (earthFiles.empty())
@@ -179,15 +265,15 @@ int main(int argc, char** argv)
     return 0;
   }
 
+  // set terrain engine
+  osgEarth::Registry::instance()->setDefaultTerrainEngineDriverName(useRex ? "rex" : "mp");
+
   osg::ref_ptr<simVis::Viewer> viewer = new simVis::Viewer();
 
   // inset view support
   simVis::View* mainView = viewer->getMainView();
   osg::ref_ptr<simVis::InsetViewEventHandler> insetHandler = new simVis::InsetViewEventHandler(mainView);
   mainView->addEventHandler(insetHandler);
-
-  // Handles hotkeys from user
-  mainView->addEventHandler(new MenuHandler(viewer, insetHandler));
 
   if (!earthFiles.empty())
   {
@@ -210,8 +296,16 @@ int main(int argc, char** argv)
   vp.range()->set(10000000, osgEarth::Units::METERS);
   mainView->tetherCamera(platformSim->platformNode(), vp, 0);
 
+  // label for elevation readout
+  osg::ref_ptr<LabelControl> elevationLabel = new LabelControl("", 14, osg::Vec4f(.8, .8, .8, 1));
+  std::shared_ptr<simUtil::MouseDispatcher> mouseDispatcher;
+  mouseDispatcher.reset(new simUtil::MouseDispatcher);
+
+  // Handles hotkeys from user
+  mainView->addEventHandler(new MenuHandler(viewer, insetHandler, elevationLabel, mouseDispatcher.get()));
+
   // show the help menu
-  viewer->getMainView()->addOverlayControl(createHelp());
+  viewer->getMainView()->addOverlayControl(createHelp(elevationLabel));
   viewer->installDebugHandlers();
   viewer->run();
 }
