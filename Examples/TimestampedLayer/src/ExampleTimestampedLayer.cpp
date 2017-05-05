95b05cdc270f20aa96e19a7a31f5070f53404939
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2017-05-05 12:08:55 -0400

    SDK NEW FEATURE: Timestamped Layer is a new example that demonstrates loading time-stamped image layers, such as for moving cloud imagery. Fixes SIM-7477.  Review: 10196

--- Examples/TimestampedLayer/src/ExampleTimestampedLayer.cpp
+++ Examples/TimestampedLayer/src/ExampleTimestampedLayer.cpp
@@ -36,61 +36,71 @@ namespace ui = osgEarth::Util::Controls;
 // Structure that holds all our application data.
 struct App
 {
-    App() : activeLayer_(NULL), firstTime_(INT_MAX), lastTime_(0) { }
+  App()
+    : activeLayer_(NULL),
+      firstTime_(INT_MAX),
+      lastTime_(0)
+  {
+  }
 
-    // UI controls
-    ui::HSliderControl* timeSlider_;
-    ui::LabelControl*   clockLabel_;
+  // UI controls
+  ui::HSliderControl* timeSlider_;
+  ui::LabelControl*   clockLabel_;
 
-    // Table that holds all timestamped layers, sorted by time:
-    typedef std::map<osgEarth::TimeStamp, osg::ref_ptr<osgEarth::VisibleLayer> > LayerTable;
-    LayerTable layers_;
+  // Table that holds all timestamped layers, sorted by time:
+  typedef std::map<osgEarth::TimeStamp, osg::ref_ptr<osgEarth::VisibleLayer> > LayerTable;
+  LayerTable layers_;
 
-    // Currently visible layer:
-    osgEarth::VisibleLayer* activeLayer_;
+  // Currently visible layer:
+  osgEarth::VisibleLayer* activeLayer_;
 
-    // Start and end times of the layer series (buffered out a little)
-    osgEarth::TimeStamp firstTime_, lastTime_;
+  // Start and end times of the layer series (buffered out a little)
+  osgEarth::TimeStamp firstTime_, lastTime_;
 
-    // Current clock time.
-    double now_;
+  // Current clock time.
+  double now_;
 
-    // Set a new clock time.
-    void setTime(double t)
+  // Set a new clock time.
+  void setTime(double t)
+  {
+    now_ = t;
+    timeSlider_->setValue(t, false);
+
+    // If we had a DataStore, we could call dataStore->update(t) here.
+
+    osgEarth::DateTime dt(t);
+    clockLabel_->setText(dt.asISO8601());
+
+    // Make the appropriate layer visible.
+    LayerTable::const_iterator i = layers_.lower_bound(t);
+    if (i != layers_.end())
     {
-        now_ = t;
-        timeSlider_->setValue(t, false);
+      if (activeLayer_ && activeLayer_ != i->second.get())
+      {
+        activeLayer_->setVisible(false);
+      }
 
-        // If we had a DataStore, we could call dataStore->update(t) here.
-        
-        osgEarth::DateTime dt(t);
-        clockLabel_->setText(dt.asISO8601());
-
-        // Make the appropriate layer visible.
-        LayerTable::const_iterator i = layers_.lower_bound(t);
-        if (i != layers_.end())
-        {
-            if (activeLayer_ && activeLayer_ != i->second.get())
-            {
-                activeLayer_->setVisible(false);
-            }
-
-            if (activeLayer_ != i->second.get())
-            {
-                activeLayer_ = i->second.get();
-                activeLayer_->setVisible(true);
-            }
-        }
+      if (activeLayer_ != i->second.get())
+      {
+        activeLayer_ = i->second.get();
+        activeLayer_->setVisible(true);
+      }
     }
+  }
 };
 
 // Callback to set the current clock using the slider
-struct ChangeTime : public ui::ControlEventHandler {
-    App& app_;
-    ChangeTime(App& app) : app_(app) { }
-    void onValueChanged(ui::Control* control, float value) {
-        app_.setTime(value);
-    }
+struct ChangeTime : public ui::ControlEventHandler
+{
+  App& app_;
+  ChangeTime(App& app)
+    : app_(app)
+  {
+  }
+  void onValueChanged(ui::Control* control, float value)
+  {
+    app_.setTime(value);
+  }
 };
 
 // Builds a UI to control the clock
@@ -104,8 +114,8 @@ ui::Control* createUI(App& app)
   // The time slider
   grid->setControl(0, row, new ui::LabelControl("Time:"));
   app.timeSlider_ = grid->setControl(1, row, new ui::HSliderControl(
-      app.firstTime_, app.lastTime_, app.firstTime_,
-      new ChangeTime(app)));
+    app.firstTime_, app.lastTime_, app.firstTime_,
+    new ChangeTime(app)));
   ++row;
 
   // The clock label
@@ -125,67 +135,71 @@ bool loadEarthFile(const std::string& earthFile, simVis::Viewer* viewer)
   osg::ref_ptr<osg::Node> node = osgDB::readNodeFile(earthFile);
   osgEarth::MapNode* mapNode = osgEarth::MapNode::get(node.get());
   if (mapNode == NULL)
-      return false;
+    return false;
 
   viewer->setMapNode(mapNode);
   return true;
 }
 
-
 // Looks in the map, finds any layer with a "time" property in ISO8601 format
 // and remembers it in the App.
 void discoverTimestampedLayers(App& app, osgEarth::Map* map)
 {
-    // collect all visible layers
-    osgEarth::VisibleLayerVector layers;
-    map->getLayers(layers);
+  typedef std::vector<osg::ref_ptr<osgEarth::VisibleLayer> > VisibleLayerVector;
 
-    for (osgEarth::VisibleLayerVector::iterator i = layers.begin(); i != layers.end(); ++i)
+  // collect all visible layers
+  VisibleLayerVector layers;
+  map->getLayers(layers);
+
+  for (VisibleLayerVector::iterator i = layers.begin(); i != layers.end(); ++i)
+  {
+    osgEarth::VisibleLayer* layer = i->get();
+
+    // Gets the serialized data so we can look for custom attributes
+    osgEarth::Config conf = layer->getConfig();
+
+    std::string iso8601 = conf.value("time");
+    // Fall back to "times" if possible
+    if (iso8601.empty())
+      iso8601 = conf.value("times");
+    if (!iso8601.empty())
     {
-        osgEarth::VisibleLayer* layer = i->get();
+      osgEarth::DateTime dt(iso8601);
 
-        // Gets the serialized data so we can look for custom attributes
-        osgEarth::Config conf = layer->getConfig();
+      // Convert to seconds post epoch
+      osgEarth::TimeStamp timestamp = dt.asTimeStamp();
 
-        std::string iso8601 = conf.value("time");
-        if (!iso8601.empty())
-        {
-            osgEarth::DateTime dt(iso8601);
+      // Compute the ealiest and latest times
+      if (timestamp < app.firstTime_)
+        app.firstTime_ = timestamp;
 
-            // Convert to seconds post epoch
-            osgEarth::TimeStamp timestamp = dt.asTimeStamp();
+      if (timestamp > app.lastTime_)
+        app.lastTime_ = timestamp;
 
-            // Compute the ealiest and latest times
-            if (timestamp < app.firstTime_)
-                app.firstTime_ = timestamp;
+      app.layers_[timestamp] = layer;
 
-            if (timestamp > app.lastTime_)
-                app.lastTime_ = timestamp;
+      SIM_NOTICE << "Found layer \"" << layer->getName()
+        << "\" with timestamp " << dt.asISO8601() << std::endl;
 
-            app.layers_[timestamp] = layer;
-
-            SIM_NOTICE << "Found layer \"" << layer->getName()
-                << "\" with timestamp " << dt.asISO8601() << std::endl;
-
-            // Start them all invisible
-            layer->setVisible(false);
-        }
+      // Start them all invisible
+      layer->setVisible(false);
     }
+  }
 
-    // Buffer the start and end times a little to accomodate the time slider.
-    if (app.layers_.size() >= 2)
-    {
-        App::LayerTable::const_iterator begin = app.layers_.begin();
-        double t0 = begin->first; begin++;
-        double t1 = begin->first;
-        app.firstTime_ -= (t1-t0);
+  // Buffer the start and end times a little to accomodate the time slider.
+  if (app.layers_.size() >= 2)
+  {
+    App::LayerTable::const_iterator begin = app.layers_.begin();
+    double t0 = begin->first; begin++;
+    double t1 = begin->first;
+    app.firstTime_ -= (t1-t0);
 
-        App::LayerTable::const_iterator end = app.layers_.end();
-        --end;
-        t1 = end->first; --end;
-        t0 = end->first;
-        app.lastTime_ += (t1-t0);        
-    }
+    App::LayerTable::const_iterator end = app.layers_.end();
+    --end;
+    t1 = end->first; --end;
+    t0 = end->first;
+    app.lastTime_ += (t1-t0);
+  }
 }
 
 
@@ -200,7 +214,7 @@ int main(int argc, char** argv)
     std::cerr << "USAGE:\n"
       << argv[0] << " <terrain.earth>\n\n"
       << "  <terrain.earth>: Earth file to load.\n\n";
-    return 0;
+    return -1;
   }
 
   osg::ref_ptr<simVis::Viewer> viewer = new simVis::Viewer();
@@ -209,7 +223,7 @@ int main(int argc, char** argv)
   // Load the earth file containing the timestamped layers:
   std::string earthFile = argv[argc-1];
   if (loadEarthFile(earthFile, viewer.get()) == false)
-      return -1;
+    return -1;
 
   // App holds all the application data.
   App app;
@@ -218,8 +232,8 @@ int main(int argc, char** argv)
   discoverTimestampedLayers(app, viewer->getSceneManager()->getMap());
   if (app.layers_.empty())
   {
-      SIM_WARN << "No timestamped layers found!\n";
-      exit(-1);
+    SIM_WARN << "No timestamped layers found!\n";
+    return -1;
   }
 
   // Install the time slider UI:
@@ -239,4 +253,5 @@ int main(int argc, char** argv)
 
     viewer->frame();
   }
+  return 0;
 }
