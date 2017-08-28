f1020f80bc9290f4727a47a1d1141003c2714dea
Martin Kinsey <kinsey@enews.nrl.navy.mil>
2017-08-28 14:43:19 -0400

    Addendum to last to fix CDash error. No review.

--- SDK/simQt/EntityLineEdit.cpp
+++ SDK/simQt/EntityLineEdit.cpp
@@ -170,7 +170,7 @@ EntityLineEdit::EntityLineEdit(QWidget* parent, simQt::EntityTreeModel* entityTr
   clock_(NULL),
   entityStateFilter_(NULL),
   state_(EntityStateFilter::BOTH),
-  settings_(NULL)
+  settings_(std::shared_ptr<simQt::Settings>())
 {
   ResourceInitializer::initialize();  // Needs to be here so that Qt Designer works.
 
