308ef820655ed2eb3fdc92096d39804d76f258b8
Colin McNulty <mcnulty@enews.nrl.navy.mil>
2017-12-29 08:33:11 -0500

    DEV: Add support for multiple groups of timed image layers.  Allows separate groups of timed imagery (i.e. satellite cloud imagery and weather radar) to be shown simultaneously and update independently.  Review 12535.
    SDK NEW FEATURE: simQt::TimestampedLayerManager now supports multiple groups of timed imagery.  At most one image from each group will be shown depending on time.  Timed image layers with no specified group will be considered in the same group.

--- SDK/simQt/TimestampedLayerManager.h
+++ SDK/simQt/TimestampedLayerManager.h
@@ -34,16 +34,22 @@ namespace osgEarth { class ImageLayer; }
 namespace simQt {
 
 /**
- * TimestampedLayerManager allows for image layers which are shown or hidden depending on the current time.
- * It watches the map for any image layers that have a time configuration and the clock for time updates.  On time updates,
- * it shows or hides the layers it finds so that only the layer with greatest time that is less than or equal to current time is shown.
- * No timed layers are shown before the earliest layer's time.
- * If image layers are set invisible by another class, this class will not set them visible.
+ * TimestampedLayerManager allows for image layers which are shown or hidden depending on the current time.  Timed layers are created by
+ * giving the layer a <time> property in the configuration file.  This property takes a UTC time reference as the layer's start time.
+ * Timed layers can also specify a group name with the <time_group> option that takes a nonempty string as the group name.  Layers with
+ * no group specified will be considered in the same group.  At most one layer from each group will be shown at a time.  This class watches the
+ * map for any image layers that have a time configuration and the clock for time updates.  On time updates, it shows or hides the layers
+ * it finds so that, for each group, only the layer with greatest time that is less than or equal to current time is shown.  No timed layers
+ * are shown before the earliest layer's time. If image layers are set invisible by another class, this class will not set them visible.
  */
 class SDKQT_EXPORT TimestampedLayerManager : public QObject
 {
   Q_OBJECT;
 public:
+
+  /// This will be used as the group name for timed layers which don't specify any group
+  static const std::string DEFAULT_LAYER_TIME_GROUP;
+
   /**
    * Constructor
    * @param clock Clock to watch for time updates
@@ -63,12 +69,16 @@ public:
    */
   bool layerIsTimed(const osgEarth::ImageLayer* layer) const;
 
+  /// Gets the time group string from the given layer.  Returns empty string if layer is not timed
+  std::string getLayerTimeGroup(const osgEarth::ImageLayer* layer);
+
   /**
-   * Returns the layer that has time value closest after the current time.
+   * Returns the layer that has time value closest after the current time for the given time group.
    * If timing is not active, the current timed layer will always be null.
+   * @param timeGroup Name of the time group to get the current layer from.
    * @return The layer that has time value closest after the current time
    */
-  const osgEarth::ImageLayer* currentTimedLayer() const;
+  const osgEarth::ImageLayer* getCurrentTimedLayer(const std::string& timeGroup = DEFAULT_LAYER_TIME_GROUP) const;
 
   /**
    * Sets the map node which will have its timed image layers managed.
@@ -93,6 +103,22 @@ signals:
   void currentTimedLayerChanged(const osgEarth::ImageLayer* newLayer = NULL, const osgEarth::ImageLayer* previousLayer = NULL);
 
 private:
+
+  /** Inner class to keep track of a time group's list of tracked layers and current layer */
+  class TimeGroup
+  {
+  public:
+    /// Constructor
+    TimeGroup();
+    /// Destructor
+    virtual ~TimeGroup();
+
+    /// All layers in this time group mapped by their start time
+    std::map<simCore::TimeStamp, osg::observer_ptr<osgEarth::ImageLayer> > layers;
+    /// Image layer with the highest time value that is less than or equal to current time
+    osg::observer_ptr<osgEarth::ImageLayer> currentLayer;
+  };
+
   /**
    * Check the given layer for time values, if any are found, adds it to the layers being watched
    * @param Layer to begin managing if time configuration is found
@@ -125,11 +151,9 @@ private:
 
   osg::ref_ptr<MapListener> mapListener_;
   std::shared_ptr<ClockListener> clockListener_;
-  std::map<simCore::TimeStamp, osg::observer_ptr<osgEarth::ImageLayer> > layers_;
+  std::map<std::string, TimeGroup*> groups_;
   /** NOTE: Keys are unowned, naked pointers.  Do not dereference. */
   std::map<const osgEarth::ImageLayer*, bool> originalVisibility_;
-  /** Image layer with the highest time value that is less than or equal to current time */
-  osg::observer_ptr<osgEarth::ImageLayer> currentLayer_;
   simCore::Clock& clock_;
   simCore::TimeStamp currTime_;
   osg::ref_ptr<osg::Node> mapChangeObserver_;
