b59d00f99c92b2e320d9ded92c83c67de4922203
George Ruhlmann <ruhlmann@enews.nrl.navy.mil>
2018-01-23 11:57:10 -0500

    SIM-8081 Update Time Widget Tool Tip
    Release Notes:  N/A
    
    
    Notes:  N/A
    
    
    Test Environment:  Windows, Visual Studio 2013, Debug, 64-bit
    
    
    Test:  See Screen Shot.
    
    
    Review: 12774

--- SDK/simQt/TimeFormatContainer.cpp
+++ SDK/simQt/TimeFormatContainer.cpp
@@ -29,6 +29,9 @@
 
 namespace simQt {
 
+static const QString USAGE_STR = QObject::tr("<p>To edit, mouse click into a field and then type new value or use the up and down arrows. Move between fields via Tab or Shift+Tab."
+    "<p>Use the right mouse click to toggle color coding.  The text is blue if the time is outside the range of the existing scenario.  The text is red if the format is invalid.");
+
 TimeFormatContainer::TimeFormatContainer(simCore::TimeFormat timeFormat, const QString& name)
   : timeFormat_(timeFormat),
   name_(name),
@@ -68,7 +71,7 @@ SecondsContainer::SecondsContainer(QWidget* parent)
   : TimeFormatContainer(simCore::TIMEFORMAT_SECONDS, "Seconds")
 {
   widget_ = new SegmentedSpinBox(parent);
-  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Sets the time in seconds since beginning of reference year.<p>Use the right mouse click to toggle color coding.<p>The text is blue if the time is outside the range of the existing scenario.  The text is red if the time format is invalid.")));
+  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Set the time in seconds since beginning of reference year.") + USAGE_STR));
 
   widget_->setLine(new SecondsTexts());
   connect(widget_->line(), SIGNAL(timeEdited(simCore::TimeStamp)), this, SIGNAL(timeEdited(simCore::TimeStamp)));
@@ -141,7 +144,7 @@ MonthContainer::MonthContainer(QWidget* parent)
     colorCode_(true)
 {
   widget_ = new SegmentedSpinBox(parent);
-  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Sets the time in Month Day Year format.<p>Use the right mouse click to toggle color coding.<p>The text is blue if the time is outside the range of the existing scenario.  The text is red if the time format is invalid.")));
+  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Set the time in Month Day Year format.") + USAGE_STR));
 
   widget_->setLine(new MonthDayYearTexts());
   connect(widget_->line(), SIGNAL(timeEdited(simCore::TimeStamp)), this, SIGNAL(timeEdited(simCore::TimeStamp)));
@@ -214,7 +217,7 @@ OrdinalContainer::OrdinalContainer(QWidget* parent)
   : TimeFormatContainer(simCore::TIMEFORMAT_ORDINAL, "Ordinal")
 {
   widget_ = new SegmentedSpinBox(parent);
-  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Sets the time in Ordinal format.<p>Use the right mouse click to toggle color coding.<p>The text is blue if the time is outside the range of the existing scenario.  The text is red if the time format is invalid.")));
+  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Set the time in Ordinal format.") + USAGE_STR));
 
   widget_->setLine(new OrdinalTexts());
   connect(widget_->line(), SIGNAL(timeEdited(simCore::TimeStamp)), this, SIGNAL(timeEdited(simCore::TimeStamp)));
@@ -288,7 +291,7 @@ MinutesContainer::MinutesContainer(QWidget* parent)
   : TimeFormatContainer(simCore::TIMEFORMAT_MINUTES, "Minutes")
 {
   widget_ = new SegmentedSpinBox(parent);
-  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Sets the time in minutes since beginning of reference year.<p>Use the right mouse click to toggle color coding.<p>The text is blue if the time is outside the range of the existing scenario.  The text is red if the time format is invalid.")));
+  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Set the time in minutes since beginning of reference year.") + USAGE_STR));
 
   widget_->setLine(new MinutesTexts());
   connect(widget_->line(), SIGNAL(timeEdited(simCore::TimeStamp)), this, SIGNAL(timeEdited(simCore::TimeStamp)));
@@ -360,7 +363,7 @@ HoursContainer::HoursContainer(QWidget* parent)
   : TimeFormatContainer(simCore::TIMEFORMAT_HOURS, "Hours")
 {
   widget_ = new SegmentedSpinBox(parent);
-  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Sets the time in hours since beginning of reference year.<p>Use the right mouse click to toggle color coding.<p>The text is blue if the time is outside the range of the existing scenario.  The text is red if the time format is invalid.")));
+  widget_->setToolTip(simQt::formatTooltip(tr("Time"), tr("Set the time in hours since beginning of reference year.") + USAGE_STR));
 
   widget_->setLine(new HoursTexts());
   connect(widget_->line(), SIGNAL(timeEdited(simCore::TimeStamp)), this, SIGNAL(timeEdited(simCore::TimeStamp)));
