a77b5cd89fd3d029236a6572b7dce37171ec3f1e
Frank Bausch <bausch@enews.nrl.navy.mil>
2018-04-11 12:02:02 -0400

    simVis: prevent unexpected outcomes using protobuf macros with null ptrs (SIM-8391)
     Release Note: N/A
    
     Notes: these are only added as they are public methods in the SDK.
    
     Test Environment: Windows 10, VC14, Debug, 64-bit.
    
     Test: compiles
    
     Review: 13477

--- SDK/simVis/Beam.cpp
+++ SDK/simVis/Beam.cpp
@@ -145,6 +145,9 @@ void BeamVolume::setBeamScale_(double beamScale)
 /// update prefs that can be updated without rebuilding the whole beam.
 void BeamVolume::performInPlacePrefChanges(const simData::BeamPrefs* a, const simData::BeamPrefs* b)
 {
+  if (a == NULL || b == NULL)
+    return;
+
   if (b->commonprefs().has_useoverridecolor() && b->commonprefs().useoverridecolor())
   {
     // Check for transition between color and override color, then check for color change
@@ -175,6 +178,9 @@ void BeamVolume::performInPlacePrefChanges(const simData::BeamPrefs* a, const si
 
 void BeamVolume::performInPlaceUpdates(const simData::BeamUpdate* a, const simData::BeamUpdate* b)
 {
+  if (a == NULL || b == NULL)
+    return;
+
   if (PB_FIELD_CHANGED(a, b, range))
   {
     SVFactory::updateFarRange(beamSV_.get(), b->range());
