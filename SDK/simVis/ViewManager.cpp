039b56db2c84b30bc5bbbbc1b2f8147f2de274f6
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2018-04-17 07:51:28 -0400

    DEV: Now setting GL_LIGHTING and GL_RESCALE_NORMAL to invalid in OSG State.  This prevents State from applying those modes, fixing invalid enumerant issues in SDK.  Fixes majority of SIM-8258 for SDK.  Review: 13526

--- SDK/simVis/ViewManager.cpp
+++ SDK/simVis/ViewManager.cpp
@@ -19,14 +19,14 @@
  * disclose, or release this software.
  *
  */
+#include <algorithm>
+#include <iterator>
+#include "osg/GLExtensions"
 #include "simNotify/Notify.h"
+#include "simVis/osgEarthVersion.h"
 #include "simVis/Registry.h"
 #include "simVis/ViewManager.h"
 #include "simVis/View.h"
-#include <iterator>
-#include <algorithm>
-
-#define LC "[ViewManager] "
 
 namespace
 {
@@ -41,6 +41,25 @@ namespace
     void operator()(osg::Object* gc_obj)
     {
       osg::GraphicsContext* gc = static_cast<osg::GraphicsContext*>(gc_obj);
+
+      // Can only call some methods on a current context
+      if (gc && gc->getState() && gc->makeCurrent())
+      {
+        const float glVersion = osg::getGLVersionNumber();
+        osg::State* state = gc->getState();
+        const unsigned int contextId = state->getContextID();
+        // One of the most reliable ways to test for core profile is to check for the
+        // compatibility profile.  If it exists, then we're not in core profile mode.
+        const bool isCoreProfile = (glVersion >= 3.2f) && !osg::isGLExtensionSupported(contextId, "GL_ARB_compatibility");
+
+        // For core profile, disable certain incompatible modes that are seen in osgEarth and loaded models
+        if (isCoreProfile)
+        {
+          state->setModeValidity(GL_LIGHTING, false);
+          state->setModeValidity(GL_RESCALE_NORMAL, false);
+        }
+      }
+
       for (unsigned int i = 0; i < viewman_->getNumViews(); ++i)
       {
         viewman_->getView(i)->processResize(gc->getTraits()->width, gc->getTraits()->height);
