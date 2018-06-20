eac15cbdf6bde16ba14d85b35ce9e263cb8e9415
Frank Bausch <bausch@enews.nrl.navy.mil>
2018-06-20 17:12:08 -0400

    simVis: Revisions to fix RFProp 3D Texture display for compatibility profile (SIM-8652)
     Release Note: N/A
    
     Notes: previosu approach did not actually compile
    
     Test Environment: Windows 10, VC14, Debug, 64-bit.
    
     Test: RFProp demo
    
     No Review: 14134

--- SDK/simVis/RF.Texture.vert.glsl
+++ SDK/simVis/RF.Texture.vert.glsl
@@ -1,10 +1,9 @@
 #version $GLSL_VERSION_STR
-$OSG_PRECISION_FLOAT
 
 #pragma vp_location vertex_view
 #pragma vp_entryPoint rf_vertex
 
-$OSG_VARYING_OUT vec2 texcoord;
+out vec2 texcoord;
 
 // Vertex shader for the 3D texture approach of RF prop visualization just assigns texture coordinate
 void rf_vertex(inout vec4 VertexVIEW)
