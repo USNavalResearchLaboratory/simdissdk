5a18a556ba3c4ff99a7ffc5622b6d69806c2f6f1
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2018-09-06 13:48:54 -0400

    SDK NEW FEATURE: simUtil::Capabilities includes a new constructor that takes an osg::GraphicsContext, in order to report capabilities on a specific graphics context instead of the global one from osgEarth Registry's Capabilities structure. Intended to help fix SIM-8662.  Review: 14969

--- SDK/simUtil/Capabilities.h
+++ SDK/simUtil/Capabilities.h
@@ -26,14 +26,29 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "osg/Texture"
 #include "simCore/Common/Common.h"
+
+namespace osg { class GraphicsContext; }
 
 namespace simUtil {
 
-/** Manages name/value pairs for graphics capabilities, useful for Help>About-like settings */
+/**
+ * Manages name/value pairs for graphics capabilities, useful for Help>About-like settings.
+ *
+ * This class can either read values from the global osgEarth Registry's Capabilities data
+ * structure, or from an arbitrary provided graphics context.  The Capabilities data structure
+ * provides a mostly accurate answer when using OSG to create the context, but has the potential
+ * to have incorrect values if the context is created outside of OSG, such as using osgQt.
+ * Because of the potential for a difference, we recommend using the GraphicsContext-supplied
+ * constructor at all times for most accurate information.
+ */
 class SDKUTIL_EXPORT Capabilities
 {
 public:
+  /** Develop capabilities for the provided graphics context. */
+  explicit Capabilities(osg::GraphicsContext& gc);
+  /** Develop capabilities using osgEarth Registry's global Capabilities structure. */
   Capabilities();
 
   /** Enumeration of usability criteria upon inspection of system capabilities */
@@ -60,8 +75,11 @@ private:
   Usability isUsable_;
   std::vector<std::string> usabilityConcerns_;
 
-  /** Initialize the capabilities vector */
+  /** Initialize the capabilities vector from osgEarth::Registry's global Capabilities. */
   void init_();
+  /** Initialize the capabilities vector from the provided graphics context. */
+  void init_(osg::GraphicsContext& gc);
+
   /** Records a concern about usability */
   void recordUsabilityConcern_(Usability severity, const std::string& concern);
   /** Extracts the OpenGL version from the GL_VERSION string */
@@ -73,7 +91,6 @@ private:
   std::string toString_(int val) const;
   /** Converts float to string */
   std::string toString_(float val) const;
-
 };
 
 }
