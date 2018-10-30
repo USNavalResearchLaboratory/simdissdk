183ce98780123859450190f9bd696f584858bc74
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2018-10-30 08:40:33 -0400

    SDK NEW FEATURE: simUtil::HudPositionManager is a new class that provides a centralized interface for positioning on-screen objects, using a callback system to change the final screen position. Intended to serve as a unification point for positioning in SIMDIS applications, allowing the end user to modify the position of on-screen window blocks.  Part of SIM-6983.  Review: 15451

---
+++ Examples/HudPositionManager/HudPositionTest.cpp
@@ -0,0 +1,363 @@
+/* -*- mode: c++ -*- */
+/****************************************************************************
+ *****                                                                  *****
+ *****                   Classification: UNCLASSIFIED                   *****
+ *****                    Classified By:                                *****
+ *****                    Declassify On:                                *****
+ *****                                                                  *****
+ ****************************************************************************
+ *
+ *
+ * Developed by: Naval Research Laboratory, Tactical Electronic Warfare Div.
+ *               EW Modeling & Simulation, Code 5773
+ *               4555 Overlook Ave.
+ *               Washington, D.C. 20375-5339
+ *
+ * License for source code at https://simdis.nrl.navy.mil/License.aspx
+ *
+ * The U.S. Government retains all rights to use, duplicate, distribute,
+ * disclose, or release this software.
+ *
+ */
+
+#include "simNotify/Notify.h"
+#include "simCore/Calc/Angle.h"
+#include "simCore/Common/Version.h"
+#include "simCore/Common/HighPerformanceGraphics.h"
+#include "simCore/String/TextReplacer.h"
+#include "simCore/String/Utils.h"
+#include "simData/MemoryDataStore.h"
+#include "simVis/ClassificationBanner.h"
+#include "simVis/Compass.h"
+#include "simVis/Viewer.h"
+#include "simUtil/ExampleResources.h"
+#include "simUtil/HudPositionManager.h"
+#include "simUtil/MapScale.h"
+#include "simUtil/Replaceables.h"
+#include "simUtil/StatusText.h"
+
+namespace ui = osgEarth::Util::Controls;
+
+static const std::string KEY_MAP_SCALE = "MapScale";
+static const std::string KEY_STATUS_TEXT = "StatusText";
+static const std::string KEY_DEMO_TEXT = "DemoText";
+static const std::string KEY_CLASSIFICATION_TOP = "ClassificationTop";
+static const std::string KEY_CLASSIFICATION_BOTTOM = "ClassificationBottom";
+static const std::string KEY_COMPASS = "Compass";
+
+//----------------------------------------------------------------------------
+
+static std::string s_title =
+  "HUD Position Manager Example \n";
+
+static std::string s_help =
+  "1 : Move 'Demo Text' to the mouse position\n"
+  "2 : Move 'Map Scale' to the mouse position\n"
+  "3 : Move 'Status Text' to mouse position\n"
+  "4 : Move 'Top Classification' to mouse position\n"
+  "5 : Move 'Bottom Classification' to mouse position\n"
+  "6 : Move 'Compass' to mouse position\n"
+  "c : Cycle classification string and color\n"
+  "r : Reset all to default positions\n"
+  "w : Toggle Wind Vane on Compass\n"
+  "z : Cycle wind angle and speed values\n"
+  ;
+
+static ui::Control* createHelp()
+{
+  // vbox is returned to caller, memory owned by caller
+  ui::VBox* vbox = new ui::VBox();
+  vbox->setPadding(10);
+  vbox->setBackColor(0, 0, 0, 0.6);
+  vbox->addControl(new ui::LabelControl(s_title, 20, simVis::Color::Yellow));
+  vbox->addControl(new ui::LabelControl(s_help, 14, simVis::Color::Silver));
+  // Move it down just a bit
+  vbox->setPosition(10, 40);
+  return vbox;
+}
+
+//----------------------------------------------------------------------------
+
+// An event handler to assist in testing the Inset functionality.
+struct MenuHandler : public osgGA::GUIEventHandler
+{
+  MenuHandler(simUtil::HudPositionManager* hud, simData::DataStore& ds)
+    : hud_(hud),
+      dataStore_(ds),
+      classificationCycle_(0),
+      windCycle_(0)
+  {
+  }
+
+  void setCompass(simVis::CompassNode* compass)
+  {
+    compass_ = compass;
+  }
+
+  bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+  {
+    if (ea.getEventType() != osgGA::GUIEventAdapter::KEYDOWN)
+      return false;
+
+    bool handled = false;
+    std::string windowName;
+    // Figure out which key was pressed
+    switch (ea.getKey())
+    {
+    case '1':
+      windowName = KEY_DEMO_TEXT;
+      break;
+    case '2':
+      windowName = KEY_MAP_SCALE;
+      break;
+    case '3':
+      windowName = KEY_STATUS_TEXT;
+      break;
+    case '4':
+      windowName = KEY_CLASSIFICATION_TOP;
+      break;
+    case '5':
+      windowName = KEY_CLASSIFICATION_BOTTOM;
+      break;
+    case '6':
+      windowName = KEY_COMPASS;
+      break;
+
+    case 'c':
+    {
+      // Cycle through a few different classification strings
+      classificationCycle_ = (++classificationCycle_ % 3);
+      simData::DataStore::Transaction txn;
+      simData::ScenarioProperties* props = dataStore_.mutable_scenarioProperties(&txn);
+      switch (classificationCycle_)
+      {
+      case 0:
+        props->mutable_classification()->set_label("UNCLASSIFIED");
+        props->mutable_classification()->set_fontcolor(0x00ff0080);
+        break;
+      case 1:
+        props->mutable_classification()->set_label("U N C L A S S I F I E D");
+        props->mutable_classification()->set_fontcolor(0xffffff80);
+        break;
+      case 2:
+        props->mutable_classification()->set_label("YOUR STRING HERE");
+        props->mutable_classification()->set_fontcolor(0xffff0080);
+        break;
+      }
+      txn.complete(&props);
+      handled = true;
+      break;
+    }
+
+    case 'r':
+      hud_->resetAllPositions();
+      handled = true;
+      break;
+
+    case 'w':
+      if (compass_.valid())
+      {
+        compass_->setWindVaneVisible(!compass_->isWindVaneVisible());
+        handled = true;
+      }
+      break;
+
+    case 'z':
+    {
+      // Cycle through a few different wind settings
+      windCycle_ = (++windCycle_ % 3);
+      simData::DataStore::Transaction txn;
+      simData::ScenarioProperties* props = dataStore_.mutable_scenarioProperties(&txn);
+      switch (windCycle_)
+      {
+      case 0:
+        props->set_windangle(35.0 * simCore::DEG2RAD);
+        props->set_windspeed(11.0);
+        break;
+      case 1:
+        props->set_windangle(282.0 * simCore::DEG2RAD);
+        props->set_windspeed(6.0);
+        break;
+      case 2:
+        props->set_windangle(179.625 * simCore::DEG2RAD);
+        props->set_windspeed(36.15698);
+        break;
+      }
+      txn.complete(&props);
+      handled = true;
+      break;
+    }
+    }
+
+    // Assign the position to the mouse's location
+    if (!windowName.empty())
+    {
+      osg::ref_ptr<simUtil::HudPositionManager> hud;
+      if (hud_.lock(hud))
+      {
+        // Rescale normalized from (-1,+1) to (0,1)
+        const osg::Vec2d pos(0.5 * (1.0 + ea.getXnormalized()),
+          0.5 * (1.0 + ea.getYnormalized()));
+        hud->setPosition(windowName, pos);
+        handled = true;
+      }
+    }
+    return handled;
+  }
+
+private:
+  osg::observer_ptr<simUtil::HudPositionManager> hud_;
+  osg::observer_ptr<simVis::CompassNode> compass_;
+  simData::DataStore& dataStore_;
+  int classificationCycle_;
+  int windCycle_;
+};
+
+int main(int argc, char** argv)
+{
+  simCore::checkVersionThrow();
+  osg::ArgumentParser arguments(&argc, argv);
+  simExamples::configureSearchPaths();
+
+  // initialize a SIMDIS viewer and load a planet.
+  osg::ref_ptr<simVis::Viewer> viewer = new simVis::Viewer(arguments);
+  viewer->setMap(simExamples::createDefaultExampleMap());
+
+  // Create a HUD position manager that will move on-screen objects
+  osg::ref_ptr<simUtil::HudPositionManager> hud = new simUtil::HudPositionManager;
+
+  // add sky node
+  simExamples::addDefaultSkyNode(viewer.get());
+  // Demonstrate the view-drawing service.  This is used to create new inset views with the mouse.
+  simVis::View* mainView = viewer->getMainView();
+  // set an initial viewpoint
+  mainView->lookAt(45, 0, 0, 0, -89, 12e6);
+
+  // Create a "Super HUD" on top of all other views and insets
+  simVis::View* superHUD = new simVis::View();
+  superHUD->setUpViewAsHUD(mainView);
+  // Add a help control
+  superHUD->addOverlayControl(createHelp());
+  mainView->getViewManager()->addView(superHUD);
+
+  // Add a classification banner
+  simData::MemoryDataStore dataStore;
+  {
+    simData::DataStore::Transaction txn;
+    simData::ScenarioProperties* props = dataStore.mutable_scenarioProperties(&txn);
+    props->mutable_classification()->set_fontcolor(0x00ff0080); // Transparent green, RRGGBBAA
+    props->mutable_classification()->set_label("UNCLASSIFIED");
+    props->set_windangle(35.0 * simCore::DEG2RAD);
+    props->set_windspeed(11.0);
+    txn.complete(&props);
+  }
+
+  // Install a handler to respond to the demo keys in this sample.
+  MenuHandler* menuHandler = new MenuHandler(hud.get(), dataStore);
+  mainView->getCamera()->addEventCallback(menuHandler);
+
+  // Configure text replacement variables that will be used for status text
+  simCore::TextReplacerPtr textReplacer(new simCore::TextReplacer());
+  textReplacer->addReplaceable(new simUtil::AzimuthVariable(mainView));
+  textReplacer->addReplaceable(new simUtil::ElevationVariable(mainView));
+  textReplacer->addReplaceable(new simUtil::LatitudeVariable(mainView));
+  textReplacer->addReplaceable(new simUtil::LongitudeVariable(mainView));
+  textReplacer->addReplaceable(new simUtil::AltitudeVariable(mainView));
+
+  {
+    // Add status text
+    osg::ref_ptr<simUtil::StatusTextNode> statusText = new simUtil::StatusTextNode(textReplacer);
+    statusText->setStatusSpec("Azim:\t%AZ%\tLat:\t%LAT%\tAlt:\t%ALT%\nElev:\t%EL%\tLon:\t%LON%\t \t \n");
+    superHUD->getOrCreateHUD()->addChild(statusText.get());
+    hud->addWindow(KEY_STATUS_TEXT, osg::Vec2d(0.005, 0.005), new simUtil::RepositionMatrixPxCallback(statusText.get()));
+    // Estimate the size.  No need to be exact at this time.
+    hud->setSize(KEY_STATUS_TEXT, osg::Vec2d(0.0, 0.0), osg::Vec2d(300, 150));
+  }
+
+  {
+    // Add a map scale
+    osg::ref_ptr<simUtil::MapScale> mapScale = new simUtil::MapScale;
+    mapScale->setView(mainView);
+    osg::MatrixTransform* xform = new osg::MatrixTransform;
+    xform->addChild(mapScale.get());
+    superHUD->getOrCreateHUD()->addChild(xform);
+    hud->addWindow(KEY_MAP_SCALE, osg::Vec2d(0.65, 0.1), new simUtil::RepositionMatrixPxCallback(xform));
+    hud->setSize(KEY_MAP_SCALE, osg::Vec2d(0.0, 0.0), osg::Vec2d(mapScale->width(), mapScale->height()));
+  }
+
+  {
+    // Add top classification text
+    osg::ref_ptr<simVis::ClassificationLabelNode> top = new simVis::ClassificationLabelNode();
+    top->bindTo(&dataStore);
+    top->setAlignment(osgText::TextBase::CENTER_TOP);
+    osg::MatrixTransform* xform = new osg::MatrixTransform();
+    xform->addChild(top.get());
+    superHUD->getOrCreateHUD()->addChild(xform);
+    hud->addWindow(KEY_CLASSIFICATION_TOP, osg::Vec2d(0.5, 0.995), new simUtil::RepositionMatrixPxCallback(xform));
+    const osg::BoundingBox& bbox = top->getBoundingBox();
+    hud->setSize(KEY_CLASSIFICATION_TOP, osg::Vec2d(bbox.xMin(), bbox.yMin()), osg::Vec2d(bbox.xMax(), bbox.yMax()));
+  }
+
+  {
+    // Add bottom classification text
+    osg::ref_ptr<simVis::ClassificationLabelNode> bottom = new simVis::ClassificationLabelNode();
+    bottom->bindTo(&dataStore);
+    bottom->setAlignment(osgText::TextBase::CENTER_BOTTOM);
+    osg::MatrixTransform* xform = new osg::MatrixTransform();
+    xform->addChild(bottom.get());
+    superHUD->getOrCreateHUD()->addChild(xform);
+    hud->addWindow(KEY_CLASSIFICATION_BOTTOM, osg::Vec2d(0.5, 0.005), new simUtil::RepositionMatrixPxCallback(xform));
+    const osg::BoundingBox& bbox = bottom->getBoundingBox();
+    hud->setSize(KEY_CLASSIFICATION_BOTTOM, osg::Vec2d(bbox.xMin(), bbox.yMin()), osg::Vec2d(bbox.xMax(), bbox.yMax()));
+  }
+
+  {
+    // Add Compass
+    osg::ref_ptr<simVis::CompassNode> compass = new simVis::CompassNode("compass.png");
+    compass->setActiveView(mainView);
+    menuHandler->setCompass(compass.get());
+    std::shared_ptr<simVis::UpdateWindVaneListener> dsUpdate(new simVis::UpdateWindVaneListener(compass.get()));
+    dataStore.addScenarioListener(dsUpdate);
+
+    // Adjust the anchor position so the compass lower-right is at (1.0,0.0)
+    static const float WIDTH_PX = 128.f;
+    static const float BUFFER_PX = 25.f;
+    const float OFFSET = WIDTH_PX * 0.5f + BUFFER_PX;
+    compass->setMatrix(osg::Matrix::translate(osg::Vec3f(-OFFSET, OFFSET, 0.f)));
+
+    // Wrap it in a transform to allow it to tie into the unified HUD easily
+    osg::MatrixTransform* xform = new osg::MatrixTransform();
+    xform->addChild(compass.get());
+    superHUD->getOrCreateHUD()->addChild(xform);
+    hud->addWindow(KEY_COMPASS, osg::Vec2d(1.0, 0.0), new simUtil::RepositionMatrixPxCallback(xform));
+    const float TOTAL_PX = WIDTH_PX + BUFFER_PX;
+    hud->setSize(KEY_COMPASS, osg::Vec2d(-TOTAL_PX, BUFFER_PX), osg::Vec2d(-BUFFER_PX, TOTAL_PX));
+  }
+
+  {
+    // Create a simple text object and wrap it in a matrix transform
+    osgText::Text* demoText = new osgText::Text;
+    demoText->setText("Demonstration osgText String");
+    demoText->setAlignment(osgText::TextBase::LEFT_BOTTOM_BASE_LINE);
+    demoText->setAxisAlignment(osgText::TextBase::SCREEN);
+    demoText->setAutoRotateToScreen(true);
+    demoText->setCharacterSize(16.f);
+    demoText->setColor(osg::Vec4f(1.f, 1.f, 1.f, 1.f));
+    demoText->setFont("arialbd.ttf");
+    demoText->setBackdropColor(osg::Vec4f(0.f, 0.f, 0.f, 1.f));
+    demoText->setBackdropType(osgText::Text::DROP_SHADOW_BOTTOM_RIGHT);
+    osg::MatrixTransform* xform = new osg::MatrixTransform();
+    xform->addChild(demoText);
+    superHUD->getOrCreateHUD()->addChild(xform);
+
+    // Add the text to the HUD at 10% / 50%
+    hud->addWindow(KEY_DEMO_TEXT, osg::Vec2d(0.1, 0.5), new simUtil::RepositionMatrixPxCallback(xform));
+    const osg::BoundingBox& bbox = demoText->getBoundingBox();
+    hud->setSize(KEY_DEMO_TEXT, osg::Vec2d(bbox.xMin(), bbox.yMin()), osg::Vec2d(bbox.xMax(), bbox.yMax()));
+  }
+
+  // for status and debugging
+  viewer->installDebugHandlers();
+
+  viewer->run();
+}
---
+++ SDK/simUtil/HudPositionManager.h
@@ -0,0 +1,198 @@
+/* -*- mode: c++ -*- */
+/****************************************************************************
+ *****                                                                  *****
+ *****                   Classification: UNCLASSIFIED                   *****
+ *****                    Classified By:                                *****
+ *****                    Declassify On:                                *****
+ *****                                                                  *****
+ ****************************************************************************
+ *
+ *
+ * Developed by: Naval Research Laboratory, Tactical Electronic Warfare Div.
+ *               EW Modeling and Simulation, Code 5770
+ *               4555 Overlook Ave.
+ *               Washington, D.C. 20375-5339
+ *
+ * For more information please send email to simdis@enews.nrl.navy.mil
+ *
+ * U.S. Naval Research Laboratory.
+ *
+ * The U.S. Government retains all rights to use, duplicate, distribute,
+ * disclose, or release this software.
+ ****************************************************************************
+ *
+ *
+ */
+#ifndef SIMUTIL_HUDPOSITIONMANAGER_H
+#define SIMUTIL_HUDPOSITIONMANAGER_H
+
+#include <map>
+#include <string>
+#include <vector>
+#include "osg/observer_ptr"
+#include "osg/ref_ptr"
+#include "osg/Referenced"
+#include "osg/Vec2d"
+#include "simCore/Common/Export.h"
+
+namespace osg { class MatrixTransform; }
+namespace osgViewer { class ViewerBase; }
+
+namespace simUtil {
+
+/**
+ * Responsible for centralizing the management of window positioning in a HUD system.  Relies
+ * on a callback system to reposition child windows.  All percentages are in the range from 0.0 to
+ * 1.0.  Position (0.0, 0.0) is in lower-left; (1.0, 1.0) is in the upper-right.
+ *
+ * This class works on an inversion of control.  It is responsible for telling windows where to
+ * position using the RepositionCallback.  Windows that register can supply a default position, but
+ * ultimately it is the interaction with this class that determines the position sent in the callback.
+ * In other words, a caller into this class can reposition your window using setPosition(), which
+ * triggers your RepositionCallback, which triggers the actual update to position.  If position updates
+ * are handled outside this flow, then they might be reverted on the next call to setPosition().
+ * Therefore, always use HudPositionManager::setPosition() to move your top level widget, relying
+ * on the values from the callback scheme.
+ *
+ * Conceptually the state of the HUD position manager can be serialized and reloaded at a later point.
+ * Modification tools such as a mouse editor can be written to manipulate positions in real-time.
+ */
+class SDKUTIL_EXPORT HudPositionManager : public osg::Referenced
+{
+public:
+  HudPositionManager();
+
+  /** Callback to be defined by windows that has them respond to position changes. */
+  class RepositionCallback : public osg::Referenced
+  {
+  public:
+    /** Reposition the window with the given X/Y percentage positions. */
+    virtual void setPosition(const std::string& name, const osg::Vec2d& positionPct) = 0;
+
+  protected:
+    /** osg::Referenced classes have protected destructors to avoid double delete. */
+    virtual ~RepositionCallback() {}
+  };
+
+  /**
+   * Adds a window with default position.  Adds a reposition callback that gets called when pos needs to update.
+   * Use this method once your window (HUD item) is defined and you have a reposition callback.  It is an error
+   * to call this method without a reposition callback.  The HUD Position Manager will notify you of correct
+   * positioning through the reposition callback.  The correct position may not be the default position that you
+   * pass in.  In most cases, you will get a call into this reposition callback before this method finishes.
+   * @param name Unique name for your window.
+   * @param defaultPositionPct Position for the window that you want to use, in percentages.  If no position was
+   *    previously specified for this window from setPosition(), then this position is used.  If a position was
+   *    previously specified using setPosition(), then the setPosition()-based position is used.  In either case,
+   *    you are notified through the repositioning callback.
+   * @param reposCallback Callback to execute when the HUD manager needs to reposition your window.  In most
+   *    cases, you will get a callback before this method returns.  This callback is stored in a ref_ptr<> and
+   *    is cleaned out after a call to removeWindow().
+   */
+  void addWindow(const std::string& name, const osg::Vec2d& defaultPositionPct, RepositionCallback* reposCallback);
+
+  /**
+   * Removes information about a window.  Position data is retained for future use.  The RepositionCallback
+   * supplied in the addWindow() call is cleared out and will no longer be executed, but users can still
+   * call getPosition() and setPosition() on the window name.  Window position data is retained in case
+   * a window is recreated, or in case of a need to serialize after window destruction.
+   * @param name Name of the window to remove.
+   * @return 0 on success, non-zero if window does not exist.
+   */
+  int removeWindow(const std::string& name);
+
+  /** Retrieves the current position for the window with given name.  Returns 0 on success, non-zero on error. */
+  int getPosition(const std::string& name, osg::Vec2d& positionPct) const;
+
+  /**
+   * Changes the position for the window with given name, calling its RepositionCallback.
+   * If the window with the given name is not yet created, the position is saved for later.
+   * This position data overrides the default position in the addWindow() call.
+   * @param name Name of a window that exists or may exist in the future
+   * @param positionPct Position of the window, in range [0,1]
+   * @return 0 on success.  It is not an error to pass in a name that does not yet exist.
+   */
+  int setPosition(const std::string& name, const osg::Vec2d& positionPct);
+
+  /** Resets the given window name to its default position. */
+  int resetPosition(const std::string& name);
+  /** Resets the position of all windows. */
+  void resetAllPositions();
+
+  /** Retrieves a list of all named windows that this GUI knows about.  Returns 0 on success, non-zero on error. */
+  int getAllWindows(std::vector<std::string>& names) const;
+
+  /** Updates the expected size of the window, in pixels (not percentages).  Returns 0 on success, non-zero on error. */
+  int getSize(const std::string& name, osg::Vec2d& minXyPx, osg::Vec2d& maxXyPx) const;
+  /** Updates the expected size of the window, in pixels (not percentages).  Returns 0 on success, non-zero on error. */
+  int setSize(const std::string& name, const osg::Vec2d& minXyPx, const osg::Vec2d& maxXyPx);
+
+protected:
+  /** osg::Referenced classes have protected destructors to avoid double delete. */
+  virtual ~HudPositionManager();
+
+private:
+  class WindowData;
+  std::map<std::string, osg::ref_ptr<WindowData> > allWindows_;
+};
+
+/** Repositions matrices that are in a 0-to-1 format */
+class SDKUTIL_EXPORT RepositionMatrixCallback : public HudPositionManager::RepositionCallback
+{
+public:
+  explicit RepositionMatrixCallback(osg::MatrixTransform* xform);
+
+  /** Overrides RepositionCallback to update the matrix transform's translation with the given values. */
+  virtual void setPosition(const std::string& name, const osg::Vec2d& positionPct);
+
+protected:
+  /** osg::Referenced classes have protected destructors to avoid double delete. */
+  virtual ~RepositionMatrixCallback();
+
+private:
+  osg::observer_ptr<osg::MatrixTransform> xform_;
+};
+
+/** Converts pixels to percentages and monitors for window resizes. */
+class SDKUTIL_EXPORT RepositionPixelsCallback : public HudPositionManager::RepositionCallback
+{
+public:
+  explicit RepositionPixelsCallback(osg::Node* node);
+
+  /** Template method for derived instances to implement to receive position values in pixels. */
+  virtual void setPositionPx(const std::string& name, const osg::Vec2d& positionPx) = 0;
+
+  /** Overrides RepositionCallback to convert the percentages to pixels. */
+  virtual void setPosition(const std::string& name, const osg::Vec2d& positionPct);
+
+protected:
+  /** osg::Referenced classes have protected destructors to avoid double delete. */
+  virtual ~RepositionPixelsCallback();
+
+private:
+  class ResizeCallback;
+  osg::observer_ptr<ResizeCallback> resizeCb_;
+  osg::observer_ptr<osg::Node> cbAttachNode_;
+  std::map<std::string, osg::Vec2d> savedPositionsPct_;
+};
+
+/** Intended to be used by osg::MatrixTransform users that reposition based on pixel values. */
+class SDKUTIL_EXPORT RepositionMatrixPxCallback : public RepositionPixelsCallback
+{
+public:
+  RepositionMatrixPxCallback(osg::MatrixTransform* xform);
+
+  /** Overrides the Reposition callback to set the matrix translate in pixels. */
+  virtual void setPositionPx(const std::string& name, const osg::Vec2d& positionPx);
+
+protected:
+  /** osg::Referenced classes have protected destructors to avoid double delete. */
+  virtual ~RepositionMatrixPxCallback();
+
+private:
+  osg::observer_ptr<osg::MatrixTransform> xform_;
+};
+
+}
+
+#endif /* SIMUTIL_HUDPOSITIONMANAGER_H */
