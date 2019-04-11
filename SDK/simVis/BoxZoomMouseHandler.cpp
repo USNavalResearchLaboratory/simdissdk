f7e800b838580e882bc8437b3d31c478ad4807b1
Martin Kinsey <kinsey@enews.nrl.navy.mil>
2019-04-11 11:27:39 -0400

    DEV: Box drawing functionality of simVis::BoxZoomMouseHandler has been abstracted into new class simVis::BoxMouseHandler. BoxZoomMouseHandler now inherits BoxMouseHandler. Part of SIM-9552, Review: 17119.

--- SDK/simVis/BoxZoomMouseHandler.cpp
+++ SDK/simVis/BoxZoomMouseHandler.cpp
@@ -33,35 +33,36 @@
 namespace simVis
 {
 
-BoxZoomMouseHandler::BoxZoomMouseHandler(const osgEarth::Util::EarthManipulator::ActionOptions& opts)
-  : goToRangeFactor_(1.0),
-    durationSec_(1.0),
-    buttonMask_(osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON),
-    modKeys_(new ModKeyHandler()),
-    cancelDragKey_(osgGA::GUIEventAdapter::KEY_Escape)
+BoxMouseHandler::BoxMouseHandler()
+  : buttonMask_(osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON),
+  modKeys_(new ModKeyHandler()),
+  cancelDragKey_(osgGA::GUIEventAdapter::KEY_Escape)
 {
-  for (auto i = opts.begin(); i != opts.end(); ++i)
-  {
-    switch (i->option())
-    {
-    case osgEarth::Util::EarthManipulator::OPTION_GOTO_RANGE_FACTOR:
-      goToRangeFactor_ = i->doubleValue();
-      break;
-    case osgEarth::Util::EarthManipulator::OPTION_DURATION:
-      durationSec_ = i->doubleValue();
-      break;
-    }
-  }
 }
 
-BoxZoomMouseHandler::~BoxZoomMouseHandler()
+BoxMouseHandler::~BoxMouseHandler()
 {
   // Just in case, remove any remnants of the box
   stopDrag_();
   delete modKeys_;
 }
 
-bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+void BoxMouseHandler::setButtonMask(int buttonMask)
+{
+  buttonMask_ = buttonMask;
+}
+
+void BoxMouseHandler::setModKeyMask(int modKeyMask)
+{
+  modKeys_->setModKeys(modKeyMask);
+}
+
+void BoxMouseHandler::setCancelDragKey(int key)
+{
+  cancelDragKey_ = key;
+}
+
+bool BoxMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
 {
   if (ea.getHandled())
   {
@@ -74,7 +75,7 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
   // Several mouse events are intercepted when we are actively dragging.
   const bool currentlyDragging = box_.valid();
   // Assertion failure means a loss of sync internally and needs fixing
-  assert(currentlyDragging == zoomView_.valid());
+  assert(currentlyDragging == view_.valid());
 
   switch (ea.getEventType())
   {
@@ -88,27 +89,27 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
     if (ea.getButtonMask() != buttonMask_ || !modKeys_->pass(ea.getModKeyMask()))
       return false;
     // Shouldn't happen, but make sure we don't leave the box laying around in an old view
-    if (box_.valid() && zoomView_.valid())
-      zoomView_->getOrCreateHUD()->removeChild(box_);
+    if (box_.valid() && view_.valid())
+      view_->getOrCreateHUD()->removeChild(box_);
 
     // Only proceed is there is a current focused view
     simVis::View* view = dynamic_cast<simVis::View*>(aa.asView());
     if (view == NULL)
       return false;
-    // Don't do anything if we don't have a map node, because the zoom won't be able to work
-    if (!mapNodeForView_(*view))
+    // Validate the view's suitability for use
+    if (!validateView_(*view))
       return false;
 
-    zoomView_ = view;
+    view_ = view;
 
     // view is unable to maintain watch or cockpit modes when box zooming
-    zoomView_->enableWatchMode(NULL, NULL);
-    zoomView_->enableCockpitMode(NULL);
+    view_->enableWatchMode(NULL, NULL);
+    view_->enableCockpitMode(NULL);
 
     originX_ = ea.getX();
     originY_ = ea.getY();
     box_ = new BoxGraphic();
-    zoomView_->getOrCreateHUD()->addChild(box_);
+    view_->getOrCreateHUD()->addChild(box_);
 
     return true;
   }
@@ -125,7 +126,7 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
     double startY = originY_;
 
     // limit drawing to within the current zoom view
-    const simVis::View::Extents& ext = zoomView_->getExtents();
+    const simVis::View::Extents& ext = view_->getExtents();
     double viewX = ext.x_;
     double viewY = ext.y_;
     double viewWidth = ext.width_;
@@ -134,7 +135,7 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
     // if view extents are a ratio of the host, convert to global extents
     if (ext.isRatio_)
     {
-      simVis::View* hostView = zoomView_->getHostView();
+      simVis::View* hostView = view_->getHostView();
       if (hostView == NULL)
       {
         // There's a view that defines its extents as ratio of host, but has no host.
@@ -175,15 +176,15 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
     // Only care if we are dragging
     if (!currentlyDragging)
       return false;
-    if (!box_.valid() || !zoomView_.valid())
+    if (!box_.valid() || !view_.valid())
     {
       // Need to reset values here to satisfy post-conditions of both being NULL when not dragging
       box_ = NULL;
-      zoomView_ = NULL;
+      view_ = NULL;
       return false;
     }
 
-    setZoom_(originX_, originY_, box_->width(), box_->height());
+    processGeometry_(originX_, originY_, box_->width(), box_->height());
 
     // done drawing the box, remove it
     stopDrag_();
@@ -210,13 +211,38 @@ bool BoxZoomMouseHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIAct
   return false;
 }
 
-void BoxZoomMouseHandler::stopDrag_()
+void BoxMouseHandler::stopDrag_()
 {
   // Remove the box
-  if (zoomView_.valid() && box_.valid())
-    zoomView_->getOrCreateHUD()->removeChild(box_);
+  if (view_.valid() && box_.valid())
+    view_->getOrCreateHUD()->removeChild(box_);
   box_ = NULL;
-  zoomView_ = NULL;
+  view_ = NULL;
+}
+
+////////////////////////////////////////////////////////////
+
+BoxZoomMouseHandler::BoxZoomMouseHandler(const osgEarth::Util::EarthManipulator::ActionOptions& opts)
+  : BoxMouseHandler(),
+    goToRangeFactor_(1.0),
+    durationSec_(1.0)
+{
+  for (auto i = opts.begin(); i != opts.end(); ++i)
+  {
+    switch (i->option())
+    {
+    case osgEarth::Util::EarthManipulator::OPTION_GOTO_RANGE_FACTOR:
+      goToRangeFactor_ = i->doubleValue();
+      break;
+    case osgEarth::Util::EarthManipulator::OPTION_DURATION:
+      durationSec_ = i->doubleValue();
+      break;
+    }
+  }
+}
+
+BoxZoomMouseHandler::~BoxZoomMouseHandler()
+{
 }
 
 void BoxZoomMouseHandler::calculateGeoPointFromScreenXY_(double x, double y, simVis::View& view, osgEarth::SpatialReference* srs, std::vector<osgEarth::GeoPoint>& points) const
@@ -228,7 +254,7 @@ void BoxZoomMouseHandler::calculateGeoPointFromScreenXY_(double x, double y, sim
   osgEarth::GeoPoint lonLatAlt(srs, 0, 0, 0, osgEarth::ALTMODE_ABSOLUTE);
   osgUtil::LineSegmentIntersector::Intersections results;
   osg::NodePath mapNodePath;
-  mapNodePath.push_back(mapNodeForView_(*zoomView_));
+  mapNodePath.push_back(mapNodeForView_(*view_));
   if (view.computeIntersections(*ea.get(), mapNodePath, results))
   {
     // find the first hit under the mouse:
@@ -239,9 +265,14 @@ void BoxZoomMouseHandler::calculateGeoPointFromScreenXY_(double x, double y, sim
   }
 }
 
-void BoxZoomMouseHandler::setZoom_(double originX, double originY, double widthPixels, double heightPixels) const
+bool BoxZoomMouseHandler::validateView_(const simVis::View& view) const
 {
-  if (!zoomView_.valid() || !mapNodeForView_(*zoomView_))
+  return (mapNodeForView_(view) != NULL);
+}
+
+void BoxZoomMouseHandler::processGeometry_(double originX, double originY, double widthPixels, double heightPixels) const
+{
+  if (!view_.valid() || !mapNodeForView_(*view_))
     return;
 
   // if box is too small, treat as a single click and center on new position
@@ -252,10 +283,10 @@ void BoxZoomMouseHandler::setZoom_(double originX, double originY, double widthP
 
     // calculate the GeoPoint from the screen coords
     std::vector<osgEarth::GeoPoint> points;
-    calculateGeoPointFromScreenXY_(originX, originY, *zoomView_, srs.get(), points);
+    calculateGeoPointFromScreenXY_(originX, originY, *view_, srs.get(), points);
     if (points.empty())
       return;
-    simVis::Viewpoint vp = zoomView_->getViewpoint();
+    simVis::Viewpoint vp = view_->getViewpoint();
     vp.focalPoint()->vec3d() = osg::Vec3d(points.front().x(), points.front().y(), 0.);
     // Adjust the range by the factor provided
     if (goToRangeFactor_ != 1.0)
@@ -267,7 +298,7 @@ void BoxZoomMouseHandler::setZoom_(double originX, double originY, double widthP
     }
     // Break tether
     vp.setNode(NULL);
-    zoomView_->setViewpoint(vp, durationSec_);
+    view_->setViewpoint(vp, durationSec_);
     return;
   }
 
@@ -276,20 +307,20 @@ void BoxZoomMouseHandler::setZoom_(double originX, double originY, double widthP
 
   // calculate the 4 corner GeoPoints from the screen coords
   std::vector<osgEarth::GeoPoint> points;
-  calculateGeoPointFromScreenXY_(originX, originY, *zoomView_, srs.get(), points);
-  calculateGeoPointFromScreenXY_(originX + widthPixels, originY, *zoomView_, srs.get(), points);
-  calculateGeoPointFromScreenXY_(originX + widthPixels, originY + heightPixels, *zoomView_, srs.get(), points);
-  calculateGeoPointFromScreenXY_(originX, originY + heightPixels, *zoomView_, srs.get(), points);
+  calculateGeoPointFromScreenXY_(originX, originY, *view_, srs.get(), points);
+  calculateGeoPointFromScreenXY_(originX + widthPixels, originY, *view_, srs.get(), points);
+  calculateGeoPointFromScreenXY_(originX + widthPixels, originY + heightPixels, *view_, srs.get(), points);
+  calculateGeoPointFromScreenXY_(originX, originY + heightPixels, *view_, srs.get(), points);
 
   // not enough points found for a reasonable zoom
   if (points.size() < 2)
     return;
 
   // use osgEarthUtil ViewFitter to create a viewpoint that encompasses the 4 corner points
-  osg::Camera* cam = zoomView_->getCamera();
+  osg::Camera* cam = view_->getCamera();
   osgEarth::Util::ViewFitter vf(srs.get(), cam);
 #if SDK_OSGEARTH_MIN_VERSION_REQUIRED(1,9,0)
-  vf.setReferenceVFOV(zoomView_->fovY());
+  vf.setReferenceVFOV(view_->fovY());
 #endif
 
   osgEarth::Viewpoint vp;
@@ -298,22 +329,7 @@ void BoxZoomMouseHandler::setZoom_(double originX, double originY, double widthP
   // prevent a case where the viewpoint can end up going under the earth
   if (vp.range().value().getValue() < 0.)
     vp.range()->set(1.0, osgEarth::Units::METERS);
-  zoomView_->setViewpoint(vp, durationSec_);
-}
-
-void BoxZoomMouseHandler::setButtonMask(int buttonMask)
-{
-  buttonMask_ = buttonMask;
-}
-
-void BoxZoomMouseHandler::setModKeyMask(int modKeyMask)
-{
-  modKeys_->setModKeys(modKeyMask);
-}
-
-void BoxZoomMouseHandler::setCancelDragKey(int key)
-{
-  cancelDragKey_ = key;
+  view_->setViewpoint(vp, durationSec_);
 }
 
 osgEarth::MapNode* BoxZoomMouseHandler::mapNodeForView_(const simVis::View& view) const
