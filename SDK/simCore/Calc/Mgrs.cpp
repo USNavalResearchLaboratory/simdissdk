9cff57d0dc6d7213408f297eefe6ec633f1e6026
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2020-02-25 10:07:10 -0500

    SDK BUGFIX: simCore::Mgrs::convertMgrstoUtm() is renamed convertMgrsToUtm() to match SDK style. DEV: CoordinateConverter.convert() is now wrapped for SWIG.  Most of Mgrs class is now wrapped.  Review: 20790

--- SDK/simCore/Calc/Mgrs.cpp
+++ SDK/simCore/Calc/Mgrs.cpp
@@ -67,7 +67,7 @@ int Mgrs::convertMgrsToGeodetic(const std::string& mgrs, double& lat, double& lo
   {
     double utmEasting;
     double utmNorthing;
-    if (convertMgrstoUtm(zone, gzdLetters, easting, northing, northPole, utmEasting, utmNorthing, err) != 0)
+    if (convertMgrsToUtm(zone, gzdLetters, easting, northing, northPole, utmEasting, utmNorthing, err) != 0)
       return 1;
     if (convertUtmToGeodetic(zone, northPole, utmEasting, utmNorthing, lat, lon, err) != 0)
       return 1;
@@ -186,7 +186,7 @@ int Mgrs::breakMgrsString(const std::string& mgrs, int& zone, std::string& gzdLe
   return 0;
 }
 
-int Mgrs::convertMgrstoUtm(int zone, const std::string& gzdLetters, double mgrsEasting, double mgrsNorthing,
+int Mgrs::convertMgrsToUtm(int zone, const std::string& gzdLetters, double mgrsEasting, double mgrsNorthing,
   bool &northPole, double& utmEasting, double& utmNorthing, std::string* err)
 {
   const double ONEHT = 100000.;
