eeb0f64646c81dcbfa4ce8e742261ba97595653c
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2020-02-27 09:50:07 -0500

    SDK BUGFIX: simQt::MapDataModel lost signals imageLayerColorFilterChanged(), imageLayerVisibleRangeChanged(), and imageLayerAltitudeChanged(), based on updates to osgEarth 3.0 API. Fixes compile errors against osgEarth 3.0.  Fixes SIM-11155.  Review: 20828

--- SDK/simQt/MapDataModel.h
+++ SDK/simQt/MapDataModel.h
@@ -154,12 +154,6 @@ signals:
   /** Qt signal as described by the signal name */
   void imageLayerOpacityChanged(osgEarth::ImageLayer* layer);
   /** Qt signal as described by the signal name */
-  void imageLayerColorFilterChanged(osgEarth::ImageLayer* layer);
-  /** Qt signal as described by the signal name */
-  void imageLayerVisibleRangeChanged(osgEarth::ImageLayer* layer);
-  /** Qt signal as described by the signal name */
-  void imageLayerAltitudeChanged(osgEarth::ImageLayer* layer);
-  /** Qt signal as described by the signal name */
   void imageLayerAdded(osgEarth::ImageLayer* layer);
   /** Qt signal as described by the signal name */
   void elevationLayerVisibleChanged(osgEarth::ElevationLayer* layer);
@@ -256,8 +250,8 @@ private: // methods
   QIcon otherIcon_;
 
   /** Maps of terrain layer callbacks */
-  QMap<osgEarth::ImageLayer*, osg::ref_ptr<osgEarth::ImageLayerCallback> > imageCallbacks_;
-  QMap<osgEarth::ElevationLayer*, osg::ref_ptr<osgEarth::ElevationLayerCallback> > elevationCallbacks_;
+  QMap<osgEarth::ImageLayer*, osg::ref_ptr<osgEarth::TileLayerCallback> > imageCallbacks_;
+  QMap<osgEarth::ElevationLayer*, osg::ref_ptr<osgEarth::TileLayerCallback> > elevationCallbacks_;
   QMap<osgEarth::FeatureModelLayer*, osg::ref_ptr<osgEarth::VisibleLayerCallback> > featureCallbacks_;
   QMap<osgEarth::VisibleLayer*, osg::ref_ptr<osgEarth::VisibleLayerCallback> > otherCallbacks_;
 
