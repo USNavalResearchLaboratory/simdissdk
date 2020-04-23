00feaf0d60cd4759434e01bf1de43a9879decb9f
Daniel Emminizer <emminizer@enews.nrl.navy.mil>
2020-04-23 11:20:33 -0400

    DEV: Various changes for public building of SWIG files alongside internal VSI building of SWIG files.  Includes: * ImportSWIG from VSI CMakeImport * Use of find_package(Python3) where appropriate * Update of variables from VSI-specific to Python3-CMake-supplied * Fix for output directory for PYD files for make test Fixes SIM-11423.  Review: 21367

---
+++ CMakeImport/ImportSWIG.cmake
@@ -0,0 +1,20 @@
+# Helper script for VSI to find SWIG executable.
+
+initialize_ENV(SWIG_DIR)
+set(SWIG_VERSION 4.0.0)
+find_program(SWIG_EXECUTABLE
+    NAMES swig
+    HINTS
+        $ENV{SWIG_DIR}/..
+        $ENV{SWIG_DIR}/bin
+        $ENV{SWIG_DIR}
+        ${THIRD_DIR}/SWIG/${SWIG_VERSION}
+        ${THIRD_DIR}/SWIG/${SWIG_VERSION}/bin
+)
+find_package(SWIG 4.0)
+
+if(SWIG_FOUND)
+    mark_as_advanced(FORCE SWIG_EXECUTABLE)
+else()
+    mark_as_advanced(CLEAR SWIG_EXECUTABLE)
+endif()
