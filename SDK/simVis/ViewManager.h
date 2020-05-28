cb7c35d64a52c680c296800ffeb1c5d9aeb093d9
Ferzam Mohammad <mohammad@enews.nrl.navy.mil>
2020-05-28 14:10:02 -0400

    DEV: Replacing header plates containing license information for source code so that they refer to new location on GitHub. Applies to SDK/simVis. Review: 21730, SIM-11493.

--- SDK/simVis/ViewManager.h
+++ SDK/simVis/ViewManager.h
@@ -9,14 +9,16 @@
  *
  *
  * Developed by: Naval Research Laboratory, Tactical Electronic Warfare Div.
- *               EW Modeling & Simulation, Code 5773
+ *               EW Modeling and Simulation, Code 5770
  *               4555 Overlook Ave.
  *               Washington, D.C. 20375-5339
  *
- * License for source code at https://simdis.nrl.navy.mil/License.aspx
+ * License for source code can be found at:
+ * https://github.com/USNavalResearchLaboratory/simdissdk/blob/master/LICENSE.txt
  *
  * The U.S. Government retains all rights to use, duplicate, distribute,
  * disclose, or release this software.
+ ****************************************************************************
  *
  */
 #ifndef SIMVIS_VIEW_MANAGER_H
