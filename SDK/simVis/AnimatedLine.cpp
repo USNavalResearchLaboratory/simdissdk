3255534daec735a6e0ce58cdb4222b97086a9aff
George Ruhlmann <ruhlmann@enews.nrl.navy.mil>
2020-08-31 11:30:01 -0400

    SIMDIS-3487 Animated Line Stays on Platform
    Release Notes: SIMDIS-3487 Animated line now stays on platform.
    
    Notes: Fixes the jitter at the second platform by dividing the long line into two lines.  The jitter now occurs at the midpoint.
    
    Test Environment: Window 10, Visual Studio 2017, Debug
    
    Test: Made an ASI file with a very long animated line.  Saw the jitter at the second platform.  No longer see the jitter after the fix.  Verified the Range Tool still works.
    
    Review: 22681

--- SDK/simVis/AnimatedLine.cpp
+++ SDK/simVis/AnimatedLine.cpp
@@ -87,6 +87,35 @@ namespace
 
 namespace simVis {
 
+AnimatedLineNode::HalfALine::HalfALine()
+{
+}
+
+AnimatedLineNode::HalfALine::~HalfALine()
+{
+}
+
+void AnimatedLineNode::HalfALine::fillSlantLine(unsigned int numSegments, const osg::Vec3d& lastPoint, bool forward)
+{
+  line1->reserve(numSegments + 1);
+  line2->reserve(numSegments + 1);
+
+  // Add points to the vertex list, from back to front, for consistent stippling.  Order
+  // matters because it affects the line direction during stippling.
+  for (unsigned int k = 0; k <= numSegments; ++k)
+  {
+    // Add in the subdivided line point
+    const double percentOfFull = forward ? (static_cast<double>(numSegments - k) / numSegments) : (static_cast<double>(k) / numSegments);
+    osg::Vec3f point = lastPoint * percentOfFull;
+    line1->pushVertex(point);
+    line2->pushVertex(point);
+  }
+
+  // Finish up
+  line1->dirty();
+  line2->dirty();
+}
+
 AnimatedLineNode::AnimatedLineNode(float lineWidth, bool depthBufferTest) :
 stipple1_(0xFF00),
 stipple2_(0x00FF),
@@ -139,7 +168,7 @@ void AnimatedLineNode::setEndPoints(const Locator* first, const Locator* second)
 
 int AnimatedLineNode::getEndPoints(simCore::MultiFrameCoordinate& coord1, simCore::MultiFrameCoordinate& coord2) const
 {
-  if (!line1_.valid())
+  if (!firstHalf_.line1.valid())
     return 1;
   coord1 = firstCoord_;
   coord2 = secondCoordMF_;
@@ -206,31 +235,55 @@ void AnimatedLineNode::initializeGeometry_()
 
   // Geode to hold the geometry.
   geode_ = new osgEarth::LineGroup();
+  firstHalf_.matrix = new osg::MatrixTransform;
+  secondHalf_.matrix = new osg::MatrixTransform;
+  geode_->addChild(firstHalf_.matrix);
+  geode_->addChild(secondHalf_.matrix);
 
   // First geometry:
   {
-    line1_ = new osgEarth::LineDrawable(GL_LINE_STRIP);
-    line1_->setName("simVis::AnimatedLine");
-    line1_->setDataVariance(osg::Object::DYNAMIC);
-    line1_->allocate(2);
-    line1_->setColor(color1_);
-    line1_->setLineWidth(lineWidth_);
-    line1_->setStipplePattern(stipple1_);
-    line1_->dirty();
-    geode_->addChild(line1_.get());
+    firstHalf_.line1 = new osgEarth::LineDrawable(GL_LINE_STRIP);
+    firstHalf_.line1->setName("simVis::AnimatedLine");
+    firstHalf_.line1->setDataVariance(osg::Object::DYNAMIC);
+    firstHalf_.line1->allocate(2);
+    firstHalf_.line1->setColor(color1_);
+    firstHalf_.line1->setLineWidth(lineWidth_);
+    firstHalf_.line1->setStipplePattern(stipple1_);
+    firstHalf_.line1->dirty();
+    firstHalf_.matrix->addChild(firstHalf_.line1.get());
+
+    firstHalf_.line2 = new osgEarth::LineDrawable(GL_LINE_STRIP);
+    firstHalf_.line2->setName("simVis::AnimatedLine");
+    firstHalf_.line2->setDataVariance(osg::Object::DYNAMIC);
+    firstHalf_.line2->allocate(2);
+    firstHalf_.line2->setColor(color2_);
+    firstHalf_.line2->setLineWidth(lineWidth_);
+    firstHalf_.line2->setStipplePattern(stipple2_);
+    firstHalf_.line2->dirty();
+    firstHalf_.matrix->addChild(firstHalf_.line2.get());
   }
 
   // Second geometry:
   {
-    line2_ = new osgEarth::LineDrawable(GL_LINE_STRIP);
-    line2_->setName("simVis::AnimatedLine");
-    line2_->setDataVariance(osg::Object::DYNAMIC);
-    line2_->allocate(2);
-    line2_->setColor(color2_);
-    line2_->setLineWidth(lineWidth_);
-    line2_->setStipplePattern(stipple2_);
-    line2_->dirty();
-    geode_->addChild(line2_.get());
+    secondHalf_.line1 = new osgEarth::LineDrawable(GL_LINE_STRIP);
+    secondHalf_.line1->setName("simVis::AnimatedLine");
+    secondHalf_.line1->setDataVariance(osg::Object::DYNAMIC);
+    secondHalf_.line1->allocate(2);
+    secondHalf_.line1->setColor(color1_);
+    secondHalf_.line1->setLineWidth(lineWidth_);
+    secondHalf_.line1->setStipplePattern(stipple1_);
+    secondHalf_.line1->dirty();
+    secondHalf_.matrix->addChild(secondHalf_.line1.get());
+
+    secondHalf_.line2 = new osgEarth::LineDrawable(GL_LINE_STRIP);
+    secondHalf_.line2->setName("simVis::AnimatedLine");
+    secondHalf_.line2->setDataVariance(osg::Object::DYNAMIC);
+    secondHalf_.line2->allocate(2);
+    secondHalf_.line2->setColor(color2_);
+    secondHalf_.line2->setLineWidth(lineWidth_);
+    secondHalf_.line2->setStipplePattern(stipple2_);
+    secondHalf_.line2->dirty();
+    secondHalf_.matrix->addChild(secondHalf_.line2.get());
   }
 
   // top-level state set sets up lighting, etc.
@@ -296,10 +349,11 @@ void AnimatedLineNode::update_(double t)
       simCore::Vec3 ecef1;
       firstLocator_->getLocatorPosition(&ecef1);
       firstLocator_->sync(firstLocatorRevision_);
-      this->setMatrix(osg::Matrix::translate(ecef1.x(), ecef1.y(), ecef1.z()));
+      firstHalf_.matrix->setMatrix(osg::Matrix::translate(ecef1.x(), ecef1.y(), ecef1.z()));
       simCore::Vec3 ecef2;
       secondLocator_->getLocatorPosition(&ecef2);
       secondLocator_->sync(secondLocatorRevision_);
+      secondHalf_.matrix->setMatrix(osg::Matrix::translate(ecef2.x(), ecef2.y(), ecef2.z()));
 
       // Perform the bendy
       drawLine_(simCore::MultiFrameCoordinate(simCore::Coordinate(simCore::COORD_SYS_ECEF, ecef1)), simCore::MultiFrameCoordinate(simCore::Coordinate(simCore::COORD_SYS_ECEF, ecef2)));
@@ -318,8 +372,7 @@ void AnimatedLineNode::update_(double t)
       const simCore::MultiFrameCoordinate coord1(simCore::Coordinate(simCore::COORD_SYS_ECEF, ecef1));
       if (locatorMoved)
       {
-        // Need to update the local matrix
-        this->setMatrix(osg::Matrix::translate(ecef1.x(), ecef1.y(), ecef1.z()));
+        firstHalf_.matrix->setMatrix(osg::Matrix::translate(ecef1.x(), ecef1.y(), ecef1.z()));
 
         // Update the coordinate reference origin.  Note that we could optimize this by
         // only setting the reference origin when the second coordinate is non-Geo (ECEF/LLA),
@@ -335,6 +388,8 @@ void AnimatedLineNode::update_(double t)
       // Resolve the second coordinate (may or may not be relative, so we need a CoordinateConverter)
       simCore::MultiFrameCoordinate secondCoordMF;
       secondCoordMF.setCoordinate(secondCoord_, *coordinateConverter_);
+      const simCore::Coordinate& ecef2 = secondCoordMF.ecefCoordinate();
+      secondHalf_.matrix->setMatrix(osg::Matrix::translate(ecef2.x(), ecef2.y(), ecef2.z()));
       drawLine_(coord1, secondCoordMF);
     }
 
@@ -349,7 +404,7 @@ void AnimatedLineNode::update_(double t)
     {
       // Reset the matrix
       const simCore::Coordinate& ecef = firstCoord_->ecefCoordinate();
-      this->setMatrix(osg::Matrix::translate(ecef.x(), ecef.y(), ecef.z()));
+      firstHalf_.matrix->setMatrix(osg::Matrix::translate(ecef.x(), ecef.y(), ecef.z()));
 
       // Need to also update the Coordinate Converter with new reference origin.  Suffers
       // the same issue as case 2 for performance here, but is less likely to be a problem
@@ -364,6 +419,8 @@ void AnimatedLineNode::update_(double t)
       // Resolve the second coordinate (may or may not be relative, so we need CoordinateConverter)
       simCore::MultiFrameCoordinate secondCoordMF;
       secondCoordMF.setCoordinate(secondCoord_, *coordinateConverter_);
+      const simCore::Coordinate& ecef = secondCoordMF.ecefCoordinate();
+      secondHalf_.matrix->setMatrix(osg::Matrix::translate(ecef.x(), ecef.y(), ecef.z()));
       drawLine_(firstCoord_, secondCoordMF);
     }
   }
@@ -373,29 +430,37 @@ void AnimatedLineNode::update_(double t)
   {
     if (useOverrideColor_)
     {
-      line1_->setColor(colorOverride_);
-      line2_->setColor(colorOverride_);
+      firstHalf_.line1->setColor(colorOverride_);
+      firstHalf_.line2->setColor(colorOverride_);
+      secondHalf_.line1->setColor(colorOverride_);
+      secondHalf_.line2->setColor(colorOverride_);
     }
     else
     {
-      line1_->setColor(color1_);
-      line2_->setColor(color2_);
+      firstHalf_.line1->setColor(color1_);
+      firstHalf_.line2->setColor(color2_);
+      secondHalf_.line1->setColor(color1_);
+      secondHalf_.line2->setColor(color2_);
     }
   }
 
   if (color1_.changed() && !useOverrideColor_)
   {
-    line1_->setColor(color1_);
+    firstHalf_.line1->setColor(color1_);
+    secondHalf_.line1->setColor(color1_);
   }
 
   if (color2_.changed() && !useOverrideColor_)
   {
-    line2_->setColor(color2_);
+    firstHalf_.line2->setColor(color2_);
+    secondHalf_.line2->setColor(color2_);
   }
 
   // LineDrawable is efficient in cases of no change
-  line1_->setLineWidth(lineWidth_);
-  line2_->setLineWidth(lineWidth_);
+  firstHalf_.line1->setLineWidth(lineWidth_);
+  firstHalf_.line2->setLineWidth(lineWidth_);
+  secondHalf_.line1->setLineWidth(lineWidth_);
+  secondHalf_.line2->setLineWidth(lineWidth_);
 
   // animate the line:
   const double dt        = t - timeLastShift_;
@@ -418,16 +483,19 @@ void AnimatedLineNode::update_(double t)
       ror(stipple2_, bits);
     }
 
-    line1_->setStipplePattern(stipple1_);
-    line2_->setStipplePattern(stipple2_);
-
+    firstHalf_.line1->setStipplePattern(stipple1_);
+    firstHalf_.line2->setStipplePattern(stipple2_);
+    secondHalf_.line1->setStipplePattern(stipple1_);
+    secondHalf_.line2->setStipplePattern(stipple2_);
     timeLastShift_ = t;
   }
   else
   {
     // process changes to stipple even if line is not animating
-    line1_->setStipplePattern(stipple1_);
-    line2_->setStipplePattern(stipple2_);
+    firstHalf_.line1->setStipplePattern(stipple1_);
+    firstHalf_.line2->setStipplePattern(stipple2_);
+    secondHalf_.line1->setStipplePattern(stipple1_);
+    secondHalf_.line2->setStipplePattern(stipple2_);
   }
 }
 
@@ -496,48 +564,26 @@ void AnimatedLineNode::drawSlantLine_(const simCore::MultiFrameCoordinate& start
   }
 
   // Reserve 2 points for the output
-  line1_->clear();
-  line2_->clear();
-
-  // Calculate the reference point in ECEF
-  const osg::Vec3d zeroPoint = this->getMatrix().getTrans();
-
-  // Convert back to ECEF and add the vertex relative to the 0 point
-  simCore::CoordinateConverter cc;
-  const simCore::Coordinate& outEcef = endPoint.ecefCoordinate();
+  firstHalf_.line1->clear();
+  firstHalf_.line2->clear();
+  secondHalf_.line1->clear();
+  secondHalf_.line2->clear();
 
   // Calculate the length of the vector
-  const osg::Vec3d lastPoint = osg::Vec3d(outEcef.x(), outEcef.y(), outEcef.z()) - zeroPoint;
-  const double distance = lastPoint.length();
-  const double segmentLength = simCore::sdkMin(distance, MAX_SEGMENT_LENGTH);
+  simCore::Vec3 delta;
+  simCore::v3Subtract(endPoint.ecefCoordinate().position(), startPoint.ecefCoordinate().position(), delta);
+  const double length = simCore::v3Length(delta);
 
-  // make sure there's enough room. Don't bother shrinking.
-  const unsigned int numSegs = simCore::sdkMax(MIN_NUM_SEGMENTS, simCore::sdkMin(MAX_NUM_SEGMENTS, static_cast<unsigned int>(distance / segmentLength)));
-  line1_->reserve(numSegs + 1);
-  line2_->reserve(numSegs + 1);
+  // Calculate the number of segments
+  const double segmentLength = simCore::sdkMin(length, MAX_SEGMENT_LENGTH);
+  unsigned int numSegs = simCore::sdkMax(MIN_NUM_SEGMENTS, simCore::sdkMin(MAX_NUM_SEGMENTS, static_cast<unsigned int>(length / segmentLength)));
+  // Easier to divide an even number of segments into two lines
+  if ((numSegs % 2) == 1)
+    ++numSegs;
 
-  // Add points to the vertex list, from back to front, for consistent stippling.  Order
-  // matters because it affects the line direction during stippling.
-  for (unsigned int k = 0; k <= numSegs; ++k)
-  {
-    // Add in the subdivided line point
-    const double percentOfFull = static_cast<double>((numSegs-k)) / numSegs; // From 1 to 0
-    osg::Vec3f point = lastPoint * percentOfFull;
-    line1_->pushVertex(point);
-    line2_->pushVertex(point);
-  }
-
-  // Finish up
-  line1_->dirty();
-  line2_->dirty();
-}
-
-void AnimatedLineNode::dirtyGeometryBounds_()
-{
-  for (unsigned int i = 0; i < geode_->getNumDrawables(); ++i)
-  {
-    geode_->getDrawable(i)->dirtyBound();
-  }
+  osg::Vec3d midPoint(delta.x() / 2.0, delta.y() / 2.0, delta.z() / 2.0);
+  firstHalf_.fillSlantLine(numSegs / 2, midPoint, true);
+  secondHalf_.fillSlantLine(numSegs / 2, -midPoint, false);
 }
 
 void AnimatedLineNode::drawBendingLine_(const simCore::MultiFrameCoordinate& coord1, const simCore::MultiFrameCoordinate& coord2)
@@ -554,28 +600,13 @@ void AnimatedLineNode::drawBendingLine_(const simCore::MultiFrameCoordinate& coo
   const simCore::Coordinate& lla2 = coord2.llaCoordinate();
 
   // Use Sodano method to calculate azimuth and distance
-  double azimuth = 0.0;
-  const double distance = simCore::sodanoInverse(lla1.lat(), lla1.lon(), lla1.alt(), lla2.lat(), lla2.lon(), &azimuth);
+  double azfwd = 0.0;
+  const double distance = simCore::sodanoInverse(lla1.lat(), lla1.lon(), lla1.alt(), lla2.lat(), lla2.lon(), &azfwd);
 
-  // Calculate the reference point in ECEF
-  const osg::Vec3d zeroPoint = this->getMatrix().getTrans();
-
-  // purely vertical line will be drawn as a single segment
+  // purely vertical line will be drawn as a slant line
   if (distance <= 0.0)
   {
-    // Convert back to ECEF and add the vertex
-    const simCore::Coordinate& outEcef = coord2.ecefCoordinate();
-    const osg::Vec3f p2 = osg::Vec3f(outEcef.x(), outEcef.y(), outEcef.z()) - zeroPoint;
-
-    for (unsigned int i = 0; i < 2; ++i)
-    {
-      osgEarth::LineDrawable* line = geode_->getLineDrawable(i);
-      line->clear();
-      line->pushVertex(p2);
-      line->pushVertex(osg::Vec3f());
-      line->dirty();
-    }
-
+    drawSlantLine_(coord1, coord2);
     return;
   }
 
@@ -588,34 +619,76 @@ void AnimatedLineNode::drawBendingLine_(const simCore::MultiFrameCoordinate& coo
     segmentLength = simCore::sdkMin(distance, MAX_SEGMENT_LENGTH_GROUNDED);
   }
 
-  // make sure there's enough room. Don't bother shrinking.
-  const unsigned int numSegs = simCore::sdkMax(MIN_NUM_SEGMENTS, simCore::sdkMin(MAX_NUM_SEGMENTS, static_cast<unsigned int>(distance / segmentLength)));
-  osg::ref_ptr<osg::Vec3Array> verts = new osg::Vec3Array();
-  verts->reserve(numSegs + 1);
+  unsigned int numSegs = simCore::sdkMax(MIN_NUM_SEGMENTS, simCore::sdkMin(MAX_NUM_SEGMENTS, static_cast<unsigned int>(distance / segmentLength)));
+  // Easier to divide an even number of segments into two lines
+  if ((numSegs % 2) == 1)
+    ++numSegs;
 
-  // Add points to the vertex list, from back to front, for consistent stippling.  Order
-  // matters because it affects the line direction during stippling.
-  for (unsigned int k = 0; k < numSegs; ++k)
+  // Point 1 to midpoint
   {
-    const float percentOfFull = (float)(numSegs-k) / (float)numSegs; // From 1 to 0 (almost 0)
+    const osg::Vec3d zeroPoint = firstHalf_.matrix->getMatrix().getTrans();
 
-    // Calculate the LLA value of the point, and replace the altitude
-    double lat = 0.0;
-    double lon = 0.0;
-    simCore::sodanoDirect(lla1.lat(), lla1.lon(), lla1.alt(), distance * percentOfFull, azimuth, &lat, &lon);
-    const double alt = lla1.alt() + percentOfFull * (lla2.alt() - lla1.alt());
+    osg::ref_ptr<osg::Vec3Array> verts = new osg::Vec3Array();
+    verts->reserve(numSegs/2 + 1);
 
-    // Convert back to ECEF and add the vertex
-    simCore::Vec3 ecefPos;
-    simCore::CoordinateConverter::convertGeodeticPosToEcef(simCore::Vec3(lat, lon, alt), ecefPos);
-    verts->push_back(osg::Vec3f(ecefPos.x(), ecefPos.y(), ecefPos.z()) - zeroPoint);
+    // start at zero
+    verts->push_back(osg::Vec3f());
+
+    // Add points to the vertex list, from back to front, for consistent stippling.  Order
+    // matters because it affects the line direction during stippling.
+    for (unsigned int k = 1; k <= numSegs/2; ++k)
+    {
+      const float percentOfFull = (float)k / (float)numSegs; // almost .0 to 0.5
+
+      // Calculate the LLA value of the point, and replace the altitude
+      double lat = 0.0;
+      double lon = 0.0;
+      simCore::sodanoDirect(lla1.lat(), lla1.lon(), lla1.alt(), distance * percentOfFull, azfwd, &lat, &lon);
+      const double alt = lla1.alt() + percentOfFull * (lla2.alt() - lla1.alt());
+
+      // Convert back to ECEF and add the vertex
+      simCore::Vec3 ecefPos;
+      simCore::CoordinateConverter::convertGeodeticPosToEcef(simCore::Vec3(lat, lon, alt), ecefPos);
+      verts->push_back(osg::Vec3f(ecefPos.x(), ecefPos.y(), ecefPos.z()) - zeroPoint);
+    }
+
+
+
+    firstHalf_.line1->importVertexArray(verts.get());
+    firstHalf_.line2->importVertexArray(verts.get());
   }
 
-  // Finish up
-  verts->push_back(osg::Vec3f());
+  // Point 2 to midpoint
+  {
+    const osg::Vec3d zeroPoint = secondHalf_.matrix->getMatrix().getTrans();
 
-  geode_->getLineDrawable(0)->importVertexArray(verts.get());
-  geode_->getLineDrawable(1)->importVertexArray(verts.get());
+    osg::ref_ptr<osg::Vec3Array> verts = new osg::Vec3Array();
+    verts->reserve(numSegs / 2 + 1);
+
+    // Add points to the vertex list, from back to front, for consistent stippling.  Order
+    // matters because it affects the line direction during stippling.
+    for (unsigned int k =  numSegs / 2; k < numSegs ; ++k)
+    {
+      const float percentOfFull = (float)(k) / (float)numSegs; // From .5 to almost 1.0
+
+      // Calculate the LLA value of the point, and replace the altitude
+      double lat = 0.0;
+      double lon = 0.0;
+      simCore::sodanoDirect(lla1.lat(), lla1.lon(), lla1.alt(), distance * percentOfFull, azfwd, &lat, &lon);
+      const double alt = lla1.alt() + percentOfFull * (lla2.alt() - lla1.alt());
+
+      // Convert back to ECEF and add the vertex
+      simCore::Vec3 ecefPos;
+      simCore::CoordinateConverter::convertGeodeticPosToEcef(simCore::Vec3(lat, lon, alt), ecefPos);
+      verts->push_back(osg::Vec3f(ecefPos.x(), ecefPos.y(), ecefPos.z()) - zeroPoint);
+    }
+
+    // Finish up
+    verts->push_back(osg::Vec3f());
+
+    secondHalf_.line1->importVertexArray(verts.get());
+    secondHalf_.line2->importVertexArray(verts.get());
+  }
 }
 
 }
