a182f68a52111875058099edc0ec68bed79883ec
Martin Kinsey <kinsey@enews.nrl.navy.mil>
2021-06-25 13:47:07 -0400

    DEV: Velocity Layer SDK example uses ImGui. SIM-12880, Review: 25279.

--- Examples/VelocityLayerTest/main.cpp
+++ Examples/VelocityLayerTest/main.cpp
@@ -22,7 +22,12 @@
  */
 #include <cmath>
 #include "osgDB/FileUtils"
+#ifdef HAVE_IMGUI
+#include "BaseGui.h"
+#include "OsgImGuiHandler.h"
+#else
 #include "osgEarth/Controls"
+#endif
 #include "simNotify/Notify.h"
 #include "simCore/Common/HighPerformanceGraphics.h"
 #include "simCore/Common/Version.h"
@@ -51,6 +56,8 @@ int usage(char* argv[])
 
   return 0;
 }
+
+#ifndef HAVE_IMGUI
 
 using namespace osgEarth::Util::Controls;
 
@@ -248,6 +255,145 @@ Control* createMenu(osgEarth::Map* map, simUtil::VelocityParticleLayer* layer)
 
 }
 
+#else
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// ImGui has this annoying habit of putting text associated with GUI elements like sliders and check boxes on
+// the right side of the GUI elements instead of on the left. Helper macro puts a label on the left instead,
+// while adding a row to a two column table started using ImGui::BeginTable(), which emulates a QFormLayout.
+// TODO: This should probably be added to osgImGuiHandler.h so that it's available to all SDK examples.
+#define IMGUI_ADD_ROW(func, label, ...) ImGui::TableNextColumn(); ImGui::Text(label); ImGui::TableNextColumn(); ImGui::SetNextItemWidth(-1); func("##" label, __VA_ARGS__)
+
+class ControlPanel : public GUI::BaseGui
+{
+public:
+  explicit ControlPanel(simUtil::VelocityParticleLayer* layer)
+    : GUI::BaseGui("Velocity Particle Layer Demo"),
+    layer_(layer)
+  {
+  }
+
+  void draw(osg::RenderInfo& ri) override
+  {
+    ImGui::SetNextWindowPos(ImVec2(15, 15));
+    ImGui::SetNextWindowBgAlpha(.6f);
+    ImGui::Begin(name(), 0, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove);
+
+    if (ImGui::BeginTable("Table", 2))
+    {
+      // Opacity
+      float opacity = layer_->getOpacity();
+      IMGUI_ADD_ROW(ImGui::SliderFloat, "Opacity", &opacity, 0.f, 1.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      if (opacity != layer_->getOpacity())
+        layer_->setOpacity(opacity);
+
+      // Num particles
+      unsigned int num = layer_->getParticleDimension();
+      ImGui::TableNextColumn(); ImGui::Text("Num Particles"); ImGui::TableNextColumn();
+      if (ImGui::RadioButton("256", num == 256)) { layer_->setParticleDimension(256); } ImGui::SameLine();
+      if (ImGui::RadioButton("512", num == 512)) { layer_->setParticleDimension(512); } ImGui::SameLine();
+      if (ImGui::RadioButton("1024", num == 1024)) { layer_->setParticleDimension(1024); } ImGui::SameLine();
+      if (ImGui::RadioButton("2048", num == 2048)) { layer_->setParticleDimension(2048); }
+
+      // Die speed
+      int dieSpeed = layer_->getDieSpeed();
+      IMGUI_ADD_ROW(ImGui::SliderInt, "Die Speed", &dieSpeed, 0, 50, "%d", ImGuiSliderFlags_AlwaysClamp);
+      if (dieSpeed != layer_->getDieSpeed())
+        layer_->setDieSpeed(dieSpeed);
+
+      // Speed
+      float speed = layer_->getSpeedFactor();
+      IMGUI_ADD_ROW(ImGui::SliderFloat, "Speed", &speed, 0.1f, 2.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      if (speed != layer_->getSpeedFactor())
+        layer_->setSpeedFactor(speed);
+
+      // Point size
+      float pointSize = layer_->getPointSize();
+      IMGUI_ADD_ROW(ImGui::SliderFloat, "Point Size", &pointSize, 1.f, 10.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      if (pointSize != layer_->getPointSize())
+        layer_->setPointSize(pointSize);
+
+      // Drop chance
+      float dropChance = layer_->getDropChance();
+      IMGUI_ADD_ROW(ImGui::SliderFloat, "Drop Chance", &dropChance, 0.f, 0.1f, "%.3f", ImGuiSliderFlags_AlwaysClamp); // 0-10% chance
+      if (dropChance != layer_->getDropChance())
+        layer_->setDropChance(dropChance);
+
+      // Altitude
+      float altitude = layer_->getParticleAltitude();
+      IMGUI_ADD_ROW(ImGui::SliderFloat, "Altitude", &altitude, 0.f, 10000.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      if (altitude != layer_->getParticleAltitude())
+        layer_->setParticleAltitude(altitude);
+
+      // Discrete gradients
+      bool isDiscrete = layer_->getGradient().isDiscrete();
+      bool newDiscrete = isDiscrete;
+      IMGUI_ADD_ROW(ImGui::Checkbox, "Discrete Colors", &newDiscrete);
+      if (isDiscrete != newDiscrete)
+      {
+        auto newGradient = layer_->getGradient();
+        newGradient.setDiscrete(newDiscrete);
+        layer_->setGradient(newGradient);
+      }
+
+      // Gradient options
+      auto setGradient = [=](const simVis::GradientShader::ColorMap& colors) {
+        simVis::GradientShader newGrad;
+        newGrad.setDiscrete(layer_->getGradient().isDiscrete());
+        newGrad.setColorMap(colors);
+        layer_->setGradient(newGrad);
+      };
+
+      ImGui::TableNextColumn(); ImGui::Text("Gradient"); ImGui::TableNextColumn();
+      if (ImGui::Button("Default")) setGradient({
+        { 0.f, osgEarth::Color::Blue },
+        { 8.f, osgEarth::Color::Cyan },
+        { 13.f, osgEarth::Color::Lime },
+        { 18.f, osgEarth::Color::Yellow },
+        { 50.f, osgEarth::Color::Red },
+        { 75.f, osgEarth::Color::Purple }
+      }); ImGui::SameLine();
+      if (ImGui::Button("Cyan")) setGradient({
+        {0.f, simVis::Color::Cyan},
+        {25.f, simVis::Color::Red}
+      }); ImGui::SameLine();
+      if (ImGui::Button("Grayscale")) setGradient({
+        {0.f, simVis::Color::Black},
+        {25.f, simVis::Color::White}
+      });
+      ImGui::TableNextColumn(); ImGui::TableNextColumn();
+      if (ImGui::Button("Green")) setGradient({
+        {0.f, simVis::Color::Lime},
+        {25.f, simVis::Color::Red}
+      }); ImGui::SameLine();
+      if (ImGui::Button("Transparent")) setGradient({
+        {0.f, osg::Vec4f(0.f, 1.f, 0.f, 0.f)},
+        {25.f, osg::Vec4f(0.f, 1.f, 0.f, 1.f)}
+      });
+
+      // Use sprites
+      bool useSprites = !layer_->getPointSprite().empty();
+      bool newUseSprites = useSprites;
+      IMGUI_ADD_ROW(ImGui::Checkbox, "Use Sprites", &newUseSprites);
+      if (useSprites != newUseSprites)
+        layer_->setPointSprite(newUseSprites ? "WindSprite.png" : "");
+
+      ImGui::EndTable();
+    }
+
+    ImGui::End();
+  }
+
+private:
+  osg::observer_ptr<simUtil::VelocityParticleLayer> layer_;
+};
+
+#endif
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[])
 {
@@ -293,10 +439,18 @@ int main(int argc, char* argv[])
   stats->getCamera()->setAllowEventFocus(false);
   mainView->addEventHandler(stats);
 
+#ifdef HAVE_IMGUI
+  // potential gotcha, need to be chained with pre-existing realize operation
+  viewMan->getViewer()->setRealizeOperation(new GUI::OsgImGuiHandler::RealizeOperation);
+  GUI::OsgImGuiHandler* gui = new GUI::OsgImGuiHandler();
+  mainView->getEventHandlers().push_front(gui);
+
+  gui->add(new ControlPanel(newLayer));
+#else
   // Add a GUI for manipulating fields
   Control* menu = createMenu(map.get(), newLayer);
   mainView->addOverlayControl(menu);
-
+#endif
   // run until the user quits by hitting ESC.
   viewMan->run();
   return 0;
