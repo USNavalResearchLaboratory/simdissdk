cf66198b96fce4d92cfd05c98715d0d48cfd14c1
Daniel Emminizer <daniel.emminizer@nrl.navy.mil>
2023-10-25 16:03:12 +0000

    SDK: Move in SIMDIS Initialization Code (1/3)
    **JIRA Issue:** SIM-16194
    
    **Description:** Migrated in simCore::initializeSimdisEnvironmentVariables() to provide executable-based initialization for SIMDIS_DIR so taht end users can run SIMDIS-related apps from the SIMDIS bin/hw-os directory without having to set environment variables for SIMDIS, osgEarth, GDAL, Proj, Qt, etc. based on a standard SIMDIS install.
    
    **Testing Performed:** Compared environment variables in SIMDIS application before and after the move to ensure nothing changed.

--- SDK/simCore/System/Utils.h
+++ SDK/simCore/System/Utils.h
@@ -41,6 +41,65 @@ SDKCORE_EXPORT std::string getExecutableFilename();
  */
 SDKCORE_EXPORT std::string getExecutablePath();
 
+
+/** Defines initialization steps to follow when setting environment variables. */
+struct InitializeEnvironmentConfig
+{
+  /** Initialize random number generator */
+  bool random = true;
+
+  /** Set SIMDIS_DIR based on executable's path */
+  bool simdisDir = true;
+  /** Print an error when SIMDIS_DIR cannot be confirmed */
+  bool cerrOnBadSimdisDir = true;
+  /** Set SIMDIS_HOME environment variable */
+  bool simdisHome = true;
+
+  /**
+   * Sentinel files for testing good expected SIMDIS_DIR, relative to SIMDIS_DIR.
+   * When simdisDir is true, these files are used as sentinels to detect good
+   * SIMDIS_DIR and whether or not to print an error with cerrOnBadSimdisDir.
+   */
+  std::vector<std::string> simdisDirSentinels = {
+    "data/fonts/arialbd.ttf",
+    "data/fonts/arial.ttf",
+    "data/fonts/times.ttf",
+  };
+
+  // All remaining values below depend on a valid SIMDIS_DIR
+
+  /** Prepend SIMDIS_DIR/bin/HW-OS to PATH */
+  bool path = true;
+
+  /** Set up Python3: PYTHONHOME, PYTHONPATH, PYTHONPYCACHEPREFIX */
+  bool python3 = true;
+  /** Python version string, used to combine to create the .ZIP and dynamic-load folders */
+  std::string python3Version = "3.11";
+
+  /** Set SIMDIS_SDK_FILE_PATH to SIMDIS_DIR/data */
+  bool simdisSdkFilePath = true;
+  /** Set SIMDIS_FONTPATH to SIMDIS_DIR/data/fonts */
+  bool simdisFontPath = true;
+  /** Set SIMDIS_TERRAIN to SIMDIS_DIR/data/sdTerrain */
+  bool simdisTerrain = true;
+  /** Set GDAL_DATA, GDAL_DRIVER_PATH, and PROJ_LIB */
+  bool gdal = true;
+
+  /** Set QT_PLUGIN_PATH, unset XDG_SESSION_TYPE */
+  bool qt = true;
+};
+
+/**
+ * Given configuration options, set various SIMDIS-related environment variables like
+ * SIMDIS_SDK_FILE_PATH, SIMDIS_DIR, QT_PLUGIN_PATH, etc. The code assumes that if
+ * the calling executable is in a good SIMDIS distribution that its path is used for
+ * SIMDIS_DIR; else it assumes SIMDIS_DIR is preset for an eventual installation path.
+ * @param config Configuration of operations to perform.
+ * @return 0 on success, non-zero on error. The only failure is if the SIMDIS_DIR
+ *   is not correctly detected based on expected files in SIMDIS_DIR.
+ */
+SDKCORE_EXPORT int initializeSimdisEnvironmentVariables(const InitializeEnvironmentConfig& config);
+
 } // namespace simCore
 
 #endif /* SIMCORE_SYSTEM_UTILS_H */
