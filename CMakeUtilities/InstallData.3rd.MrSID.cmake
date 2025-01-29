5998c385ac30cfcbc5db7346ce4445c6ec48c637
Daniel Emminizer <daniel.m.emminizer.civ@us.navy.mil>
2025-01-29 13:32:39 +0000

    SDK: Fix Third Party Library Issues with Standalone Build
    **JIRA Issue:** SIM-17878
    
    **Description:** Fixed build issues identified with exported build and third party pack without PEOPLE_DIR:
    * MrSID distro doesn't have include files, only runtimes; so better search for root directories
    * MrSID no longer attempts to install if the directory is not found
    * Like protobuf a few lines up, simQt only uses OSG for the build install path, to fix an EXPORT issue with simQt using a path in source tree
    
    **Testing Performed:** Generated distro, and built and installed it both with PEOPLE_DIR defined and not defined, using VSI and public defaults both.

--- CMakeUtilities/InstallData.3rd.MrSID.cmake
+++ CMakeUtilities/InstallData.3rd.MrSID.cmake
@@ -1,10 +1,16 @@
 find_path(MRSID_RASTER_DIR
-    NAMES include/MrSIDImageReader.h
+    NAMES
+        include/MrSIDImageReader.h
+        lib/libltidsdk.so
+        lib/lti_dsdk.lib
     PATH_SUFFIXES Raster_DSDK
 )
 
 find_path(MRSID_LIDAR_DIR
-    NAMES include/lidar/MG4PointReader.h
+    NAMES
+        include/lidar/MG4PointReader.h
+        lib/liblti_lidar_dsdk.so
+        lib/lti_lidar_dsdk.lib
     PATH_SUFFIXES Lidar_DSDK
 )
 
@@ -24,27 +30,29 @@ macro(mrsid_install_library PATH NAME)
 endmacro()
 
 # Install each library
-if(WIN32)
-    mrsid_install_library(MRSID_RASTER_DIR lti_dsdk_9.5.dll)
-    if(INSTALL_MRSID_TBB OR NOT DEFINED INSTALL_MRSID_TBB)
-        mrsid_install_library(MRSID_RASTER_DIR tbb.dll)
+if(MRSID_RASTER_DIR AND IS_DIRECTORY "${MRSID_RASTER_DIR}")
+    if(WIN32)
+        mrsid_install_library(MRSID_RASTER_DIR lti_dsdk_9.5.dll)
+        if(INSTALL_MRSID_TBB OR NOT DEFINED INSTALL_MRSID_TBB)
+            mrsid_install_library(MRSID_RASTER_DIR tbb.dll)
+        endif()
+    else()
+        mrsid_install_library(MRSID_RASTER_DIR libltidsdk.so)
+        if(INSTALL_MRSID_TBB OR NOT DEFINED INSTALL_MRSID_TBB)
+            mrsid_install_library(MRSID_RASTER_DIR libtbb.so)
+        endif()
     endif()
-    mrsid_install_library(MRSID_LIDAR_DIR lti_lidar_dsdk_1.1.dll)
-else()
-    mrsid_install_library(MRSID_RASTER_DIR libltidsdk.so)
-    if(INSTALL_MRSID_TBB OR NOT DEFINED INSTALL_MRSID_TBB)
-        mrsid_install_library(MRSID_RASTER_DIR libtbb.so)
-    endif()
-    mrsid_install_library(MRSID_LIDAR_DIR liblti_lidar_dsdk.so)
-endif()
-
-# Mark variables as advanced
-if(IS_DIRECTORY "${MRSID_RASTER_DIR}")
     mark_as_advanced(FORCE MRSID_RASTER_DIR)
 else()
     mark_as_advanced(CLEAR MRSID_RASTER_DIR)
 endif()
-if(IS_DIRECTORY "${MRSID_LIDAR_DIR}")
+
+if(MRSID_LIDAR_DIR AND IS_DIRECTORY "${MRSID_LIDAR_DIR}")
+    if(WIN32)
+        mrsid_install_library(MRSID_LIDAR_DIR lti_lidar_dsdk_1.1.dll)
+    else()
+         mrsid_install_library(MRSID_LIDAR_DIR liblti_lidar_dsdk.so)
+    endif()
     mark_as_advanced(FORCE MRSID_LIDAR_DIR)
 else()
     mark_as_advanced(CLEAR MRSID_LIDAR_DIR)
