e9401c6091ba88e13469602c65e901f921b00811
Daniel Emminizer <3845830+emminizer@users.noreply.github.com>
2025-05-20 06:03:32 -0400

    Update AddExecutablePostfix.cmake
    Spacing fixes

--- CMakeModules/AddExecutablePostfix.cmake
+++ CMakeModules/AddExecutablePostfix.cmake
@@ -1,19 +1,19 @@
 # Provide an implementation of add_executable that applies a debug postfix
 
-if (USING_VCPKG)
-    message(STATUS "VCPKG is enabled disable override of add_executable")
+if(USING_VCPKG)
+    message(STATUS "VCPKG is enabled; disable add_executable() modification to add debug postfix")
 else()
     function(add_executable TARGET)
-    _add_executable(${TARGET} ${ARGN})
-    get_target_property(IS_IMPORTED ${TARGET} IMPORTED)
-    if(NOT IS_IMPORTED)
-        set_target_properties(${TARGET} PROPERTIES
-            DEBUG_POSTFIX "${CMAKE_DEBUG_POSTFIX}"
-            RELEASE_POSTFIX "${CMAKE_RELEASE_POSTFIX}"
-            RELWITHDEBINFO_POSTFIX "${CMAKE_RELWITHDEBINFO_POSTFIX}"
-            MINSIZEREL_POSTFIX "${CMAKE_MINSIZEREL_POSTFIX}"
-        )
-    endif()
-endfunction()
+        _add_executable(${TARGET} ${ARGN})
+        get_target_property(IS_IMPORTED ${TARGET} IMPORTED)
+        if(NOT IS_IMPORTED)
+            set_target_properties(${TARGET} PROPERTIES
+                DEBUG_POSTFIX "${CMAKE_DEBUG_POSTFIX}"
+                RELEASE_POSTFIX "${CMAKE_RELEASE_POSTFIX}"
+                RELWITHDEBINFO_POSTFIX "${CMAKE_RELWITHDEBINFO_POSTFIX}"
+                MINSIZEREL_POSTFIX "${CMAKE_MINSIZEREL_POSTFIX}"
+            )
+        endif()
+    endfunction()
 endif()
 
