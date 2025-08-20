ed40cb374acacc1b1da4464f50a5af9ef84f7801
Daniel Emminizer <daniel.m.emminizer.civ@us.navy.mil>
2025-08-20 19:50:09 +0000

    SDK: osgQOpenGL Qt5 Fix (1/3)
    **JIRA Issue:** SIM-18648
    
    **Description:** Update naming for osgQOpenGL to include the Qt version (osgQOpenGL-qt5). Preparing for Qt6 support.
    
    **Testing Performed:** Full build and run on MSVC. Ran PythonZip to confirm distribution good.

--- CMakeUtilities/InstallData.3rd.osgQOpenGL.cmake
+++ CMakeUtilities/InstallData.3rd.osgQOpenGL.cmake
@@ -1,17 +1,17 @@
-if(NOT TARGET osgQt::osgQOpenGL)
+if(NOT TARGET osgQt::osgQOpenGL-qt5)
     return()
 endif()
 
 if(WIN32)
-    vsi_install_target(osgQt::osgQOpenGL ThirdPartyLibs)
+    vsi_install_target(osgQt::osgQOpenGL-qt5 ThirdPartyLibs)
     return()
 endif()
 
-# Need the library directory to get symlinks from libosgQOpenGL.so
-get_target_property(_OSGQOPENGL_SO osgQt::osgQOpenGL IMPORTED_LOCATION_RELEASE)
+# Need the library directory to get symlinks from libosgQOpenGL-qt5.so
+get_target_property(_OSGQOPENGL_SO osgQt::osgQOpenGL-qt5 IMPORTED_LOCATION_RELEASE)
 if(NOT _OSGQOPENGL_SO)
-    get_target_property(_OSGQOPENGL_SO osgQt::osgQOpenGL IMPORTED_LOCATION)
+    get_target_property(_OSGQOPENGL_SO osgQt::osgQOpenGL-qt5 IMPORTED_LOCATION)
 endif()
 get_filename_component(_OSGQOPENGL_LIB_DIR "${_OSGQOPENGL_SO}" DIRECTORY)
-get_symlinks("${_OSGQOPENGL_LIB_DIR}/libosgQOpenGL.so" _OSGQOPENGL_LIBS)
+get_symlinks("${_OSGQOPENGL_LIB_DIR}/libosgQOpenGL-qt5.so" _OSGQOPENGL_LIBS)
 install(PROGRAMS ${_OSGQOPENGL_LIBS} DESTINATION "${INSTALLSETTINGS_SHARED_LIBRARY_DIR}" OPTIONAL COMPONENT ThirdPartyLibs)
