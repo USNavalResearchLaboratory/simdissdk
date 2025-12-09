f9dfc53d03951daf3eb8148fe270adfcee8f53be
George Ruhlmann <george.m.ruhlmann.ctr@us.navy.mil>
2025-12-09 15:58:05 +0000

    Added Less Than Operator For simData::ReflectionValue
    **JIRA Issue:** SIM-19123
    
    **Description:** Needed for Rocky Demo Qt.  
    
    **Testing Performed:** Updated and ran unit test

--- SDK/simData/DataTypeReflection.cpp
+++ SDK/simData/DataTypeReflection.cpp
@@ -166,6 +166,42 @@ bool ReflectionValue::operator!=(const ReflectionValue& rhs) const
   return !operator==(rhs);
 }
 
+bool ReflectionValue::operator<(const ReflectionValue& rhs) const
+{
+  if (type() != rhs.type())
+  {
+    // The types should be the same
+    assert(false);
+    return type() < rhs.type();
+  }
+
+  if (std::holds_alternative<StringPtr>(data_))
+  {
+    const auto& lhsString = getString();
+    const auto& rhsString = rhs.getString();
+    if (lhsString != rhsString)
+      return lhsString < rhsString;
+  }
+
+  if (std::holds_alternative<StringVectorPtr>(data_))
+  {
+    const auto& lhsStrings = getStrings();
+    const auto& rhsStrings = rhs.getStrings();
+    if (lhsStrings != rhsStrings)
+      return lhsStrings < rhsStrings;
+  }
+
+  if (std::holds_alternative<IdVectorPtr>(data_))
+  {
+    const auto& lhsIds = getIds();
+    const auto& rhsIds = rhs.getIds();
+    if (lhsIds != rhsIds)
+      return lhsIds < rhsIds;
+  }
+
+  return data_ < rhs.data_;
+}
+
 bool ReflectionValue::getBoolean() const
 {
   try
