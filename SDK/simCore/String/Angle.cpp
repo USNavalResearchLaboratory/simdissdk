f8d2f95f8821f8258125b84756bd723b1bbec1db
George Ruhlmann <george.m.ruhlmann.ctr@us.navy.mil>
2025-12-19 14:15:50 +0000

    Implemented Performance Improvements to simCore::getAngleString
    **JIRA Issue:** SIM-19170
    
    **Description:** Implemented the improvements by making a reusable class.  Replaced std::stringstream with std::to_chars and the results were dramatic. Fortunately, the unit tests are complete.  There were edge cases with precision that I needed to address.  I was able to remove redundant calculations such as calls to “fix” the angles and calls to std::pow().  I attempted to remove the negative “dance”, but I could not.  
      
    **Notes:** The original benchmark of 33542145 calls for the simCore::getAngleString method was 29.8 seconds and the improved benchmark is 6.99 seconds or about 4.26 times faster.   The benchmark directly using the new class configured once was 2.1 seconds or about 14.2 times faster.  
    
    **Testing Performed:** Unit test still passes.

--- SDK/simCore/String/Angle.cpp
+++ SDK/simCore/String/Angle.cpp
@@ -20,11 +20,12 @@
  * disclose, or release this software.
  *
  */
-#include <string>
-#include <vector>
+
+#include <charconv>
 #include <cstdlib>
 #include <iomanip>
-
+#include <string>
+#include <vector>
 #include "simCore/Calc/Angle.h"
 #include "simCore/Calc/Math.h"
 #include "simCore/Calc/Units.h"
@@ -139,178 +140,14 @@ const simCore::Units& formatToUnits(GeodeticFormat format)
 std::string getAngleString(double radianAngle, GeodeticFormat format, bool allNumerics,
   size_t precision, simCore::DegreeSymbolFormat degSymbol, char positiveDir, char negativeDir, bool allowRollover)
 {
-  double degreeAngle = simCore::Units::RADIANS.convertTo(simCore::Units::DEGREES, radianAngle);
-  precision = simCore::sdkMin(precision, static_cast<size_t>(16));
-
-  std::string hemiDir = " ";
-  bool negative = false;
-  if (degreeAngle < 0.0)
-  {
-    if (negativeDir == '\0')
-      hemiDir.clear();
-    else
-      hemiDir += negativeDir;
-    degreeAngle = -degreeAngle;
-    negative = true;
-  }
-  else
-  {
-    if (positiveDir == '\0')
-      hemiDir.clear();
-    else
-      hemiDir += positiveDir;
-  }
-
-  std::string degreeSymbolString;
-  std::string minuteSymbolString;
-  std::string secondSymbolString;
-
-  if (!allNumerics)
-  {
-    degreeSymbolString = simCore::getDegreeSymbol(degSymbol);
-    minuteSymbolString = "'";
-    secondSymbolString = "\"";
-  }
-  else
-  {
-    hemiDir.clear();
-  }
-
-  const bool printNegativeSign = (allNumerics || negativeDir == '\0');
-
-  const double tolerance = 0.00000001; //< Value to avoid instance of -0
-  double minValue = 0;  // DM min value
-  std::string angleString;
-  switch (format)
-  {
-  case FMT_DEGREES_MINUTES_SECONDS:
-  {
-    // extract minutes as fraction of degrees
-    minValue = (degreeAngle - floor(degreeAngle)) * 60.0;
-
-    // truncate degrees
-    degreeAngle = floor(degreeAngle);
-
-    // same for seconds on minutes, with rounding
-    double secValue = (minValue - floor(minValue)) * 60.0;
-    minValue = floor(minValue);
-
-    // Don't permit negative seconds
-    if (secValue < tolerance)
-      secValue = 0.0;
-
-    const double rounding = 5.0 / pow(10.0, precision + 1.0);
-    if ((secValue + rounding > 60.0) || simCore::areEqual(secValue + rounding, 60.0))
-    {
-      secValue = 0.0;
-      minValue += 1.0;
-      if (minValue >= 60.0)
-      {
-        minValue = 0.0;
-        degreeAngle += 1.0;
-      }
-    }
-    else
-    {
-      double fraction = fmod(secValue * pow(10.0, precision + 1.0), 10.0);
-      if ((fraction > 5.0) || simCore::areEqual(fraction, 5.0))
-      {
-        secValue += rounding;
-      }
-    }
-
-    // degreeAngle was floor() above so just checking for 360 is OK
-    if (degreeAngle == 360.0)
-      degreeAngle = 0.0;
-    degreeAngle = (negative && printNegativeSign) ? -degreeAngle : degreeAngle;
-
-    std::stringstream strDegMinSec;
-    // SIM-14416: force leading -, as stringstream won't handle -0 as -0
-    if (negative && printNegativeSign && degreeAngle == -0.)
-      strDegMinSec << '-';
-    strDegMinSec << static_cast<int>(degreeAngle) << degreeSymbolString << " ";
-    strDegMinSec.setf(std::ios::fixed, std::ios::floatfield);
-    strDegMinSec << std::setfill('0') << std::setw(2) << static_cast<int>(minValue) << minuteSymbolString << " ";
-    const size_t width = (precision == 0 ? (2) : (precision + 3));  // Account for decimal if precision is non-zero
-    strDegMinSec << std::setfill('0') << std::setw(width) << std::setprecision(precision) << secValue << secondSymbolString;
-    angleString = strDegMinSec.str();
-  }
-  break;
-
-  case FMT_DEGREES_MINUTES:
-  {
-    minValue = ((degreeAngle - floor(degreeAngle)) * 60.0);
-    degreeAngle = floor(degreeAngle);
-
-    if (fabs(minValue) <= tolerance)
-      minValue = 0.0;
-
-    const double rounding = 5.0 / pow(10.0, precision + 1.0);
-    if (minValue + rounding >= 60.0 || simCore::areEqual(minValue + rounding, 60.0))
-    {
-      degreeAngle += 1.0;
-      minValue = 0.0;
-    }
-    else
-    {
-      double fraction = fmod(minValue * pow(10.0, precision + 1.0), 10.0);
-      if ((fraction > 5.0) || simCore::areEqual(fraction, 5.0))
-      {
-        minValue += rounding;
-      }
-    }
-
-    // degreeAngle was floor() above so just checking for 360 is OK
-    if (degreeAngle == 360.0)
-      degreeAngle = 0.0;
-    degreeAngle = (negative && printNegativeSign) ? -degreeAngle : degreeAngle;
-
-    std::stringstream strDegMin;
-    // SIM-14416: force leading -, as stringstream won't handle -0 as -0
-    if (negative && printNegativeSign && degreeAngle == -0.)
-      strDegMin << '-';
-    strDegMin << static_cast<int>(degreeAngle) << degreeSymbolString << " ";
-    strDegMin.setf(std::ios::fixed, std::ios::floatfield);
-    size_t width = (precision == 0 ? (2) : (precision + 3));  // Account for decimal if precision is non-zero
-    strDegMin << std::setfill('0') << std::setw(width) << std::setprecision(precision) << minValue << minuteSymbolString;
-    angleString = strDegMin.str();
-  }
-  break;
-
-  case FMT_RADIANS:
-  case FMT_BAM:
-  case FMT_MIL:
-  case FMT_MILLIRADIANS:
-  {
-    degreeAngle = (negative && printNegativeSign) ? -degreeAngle : degreeAngle;
-    std::stringstream str;
-    str.setf(std::ios::fixed, std::ios::floatfield);
-    str << std::setprecision(precision) << simCore::Units::DEGREES.convertTo(formatToUnits(format), degreeAngle);
-    angleString = str.str();
-  }
-  break;
-
-  case FMT_DEGREES:
-  default:
-  {
-    if (!allowRollover)
-    {
-      const double rounding = 5.0 / pow(10.0, precision + 1.0);
-      degreeAngle = simCore::angFix360(degreeAngle);
-      if (fabs(simCore::angleDifferenceDeg(degreeAngle, 360.0)) < rounding)
-        degreeAngle = 0.0;
-    }
-
-    degreeAngle = (negative && printNegativeSign) ? -degreeAngle : degreeAngle;
-    std::stringstream str;
-    str.setf(std::ios::fixed, std::ios::floatfield);
-    str << std::setprecision(precision) << degreeAngle << degreeSymbolString;
-    angleString = str.str();
-  }
-  break;
-  }
-
-  return angleString + hemiDir;
+  AngleFormatter formatter;
+  formatter.setFormat(format);
+  formatter.setAllNumerics(allNumerics);
+  formatter.setPrecision(precision);
+  formatter.setSymbol(degSymbol);
+  formatter.setDir(positiveDir, negativeDir);
+  formatter.setAllowRollover(allowRollover);
+  return formatter.format(radianAngle);
 }
 
 /// convert lat (in radians) to string in the form: ##.####### N/S
@@ -327,6 +164,356 @@ std::string printLongitude(double lonRadians, GeodeticFormat format, bool allNum
 {
   lonRadians = simCore::angFixPI(lonRadians);
   return getAngleString(lonRadians, format, allNumerics, precision, degSymbol, positiveDir, negativeDir);
+}
+
+//---------------------------------------------------------------------------------------------------------------
+
+AngleFormatter::AngleFormatter()
+{
+  printNegativeSign_ = getPrintNegativeSign_();
+  setSymbols_();
+  calculateScales_();
+}
+
+AngleFormatter::~AngleFormatter()
+{
+}
+
+void AngleFormatter::setFormat(GeodeticFormat format)
+{
+  format_ = format;
+}
+
+void AngleFormatter::setAllNumerics(bool allNumerics)
+{
+  if (allNumerics_ == allNumerics)
+    return;
+
+  allNumerics_ = allNumerics;
+  printNegativeSign_ = getPrintNegativeSign_();
+
+  setSymbols_();
+}
+
+void AngleFormatter::setPrecision(size_t precision)
+{
+  const size_t limitedPrecision = simCore::sdkMin(precision, static_cast<size_t>(16));
+  if (limitedPrecision == precision_)
+    return;
+
+  precision_ = limitedPrecision;
+  calculateScales_();
+}
+
+void AngleFormatter::setSymbol(DegreeSymbolFormat degSymbol)
+{
+  if (degSymbol_ == degSymbol)
+    return;
+
+  degSymbol_ = degSymbol;
+  if (!allNumerics_)
+    degreeSymbolString_ = simCore::getDegreeSymbol(degSymbol_);
+}
+
+void AngleFormatter::setDir(char positiveDir, char negativeDir)
+{
+  if (positiveDir == '\0')
+    positiveDir_.clear();
+  else
+    positiveDir_ = positiveDir;
+
+  if (negativeDir == '\0')
+    negativeDir_.clear();
+  else
+    negativeDir_ = negativeDir;
+
+  printNegativeSign_ = getPrintNegativeSign_();
+}
+
+void AngleFormatter::setAllowRollover(bool allowRollover)
+{
+  allowRollover_ = allowRollover;
+}
+
+std::string AngleFormatter::format(double radianAngle) const
+{
+  switch (format_)
+  {
+  case FMT_DEGREES:
+    return formatDegrees_(radianAngle);
+
+  case FMT_DEGREES_MINUTES:
+    return formatDegreesMinutes_(radianAngle);
+
+  case FMT_DEGREES_MINUTES_SECONDS:
+    return formatDegreesMinutesSeconds_(radianAngle);
+
+  case FMT_RADIANS:
+    return formatRadians_(radianAngle);
+
+  case FMT_BAM:
+  case FMT_MIL:
+  case FMT_MILLIRADIANS:
+    return formatMiscellaneous_(radianAngle);
+  }
+
+  return "";
+}
+
+std::string AngleFormatter::formatDegrees_(double radianAngle) const
+{
+  const bool wasNegative = (radianAngle < 0.0);
+  if (wasNegative)
+    radianAngle = -radianAngle;
+
+  if (!allowRollover_)
+  {
+    if (fabs(simCore::angleDifference(radianAngle, M_TWOPI)) < radiansTolerance_)
+      radianAngle = 0.0;
+    else
+      radianAngle = simCore::angFix2PI(radianAngle);
+  }
+
+  if (printNegativeSign_ && wasNegative)
+    radianAngle = -radianAngle;
+
+  std::string angleString;
+  appendDouble_(angleString, radianAngle * simCore::RAD2DEG, precision_);
+  angleString += degreeSymbolString_;
+  appendHemisphereDirection_(angleString, wasNegative);
+
+  return angleString;
+}
+
+std::string AngleFormatter::formatDegreesMinutes_(double radianAngle) const
+{
+  const bool wasNegative = (radianAngle < 0.0);
+
+  double degreeValue = simCore::Units::RADIANS.convertTo(simCore::Units::DEGREES, radianAngle);
+  if (wasNegative)
+    degreeValue = -degreeValue;
+
+  // extract minutes as fraction of degrees
+  double minuteValue = (degreeValue - floor(degreeValue)) * 60.0;
+
+  // truncate degrees
+  degreeValue = floor(degreeValue);
+
+  // Don't permit negative seconds
+  const double tolerance = 0.00000001; //< Value to avoid instance of -0
+  if (fabs(minuteValue) < tolerance)
+    minuteValue = 0.0;
+
+  if ((minuteValue + degreesTolerance_ > 60.0) || simCore::areEqual(minuteValue + degreesTolerance_, 60.0))
+  {
+    degreeValue += 1.0;
+    minuteValue = 0.0;
+  }
+  else
+  {
+    double fraction = fmod(minuteValue * scale_, 10.0);
+    if (fraction > 5.0)
+      minuteValue += degreesTolerance_;
+    else if (simCore::areEqual(fraction, 5.0))
+      minuteValue += degreesTolerance_ + 0.000001;
+  }
+
+  // degreeAngle was floor() above so just checking for 360 is OK
+  if (degreeValue == 360.0)
+    degreeValue = 0.0;
+  degreeValue = (wasNegative && printNegativeSign_) ? -degreeValue : degreeValue;
+
+  std::string angleString;
+
+  // SIM-14416: force leading -, as stringstream won't handle -0 as -0
+  if (wasNegative && printNegativeSign_ && degreeValue == -0.)
+    angleString = "-";
+  angleString += std::to_string(static_cast<int>(degreeValue));
+  angleString += degreeSymbolString_;
+  angleString += " ";
+
+  appendPadded_(angleString, static_cast<int>(floor(minuteValue)), 2);
+  if (precision_ > 0)
+  {
+    double fractionalPart = minuteValue - floor(minuteValue);
+    std::string fracStr;
+    appendDouble_(fracStr, fractionalPart, precision_);
+    angleString += (fracStr.substr(1, precision_ + 1)); // skip '0' and take '.' + precision digits
+  }
+  angleString += (minuteSymbolString_);
+
+  appendHemisphereDirection_(angleString, wasNegative);
+
+  return angleString;
+}
+
+std::string AngleFormatter::formatDegreesMinutesSeconds_(double radianAngle) const
+{
+  const bool wasNegative = (radianAngle < 0.0);
+
+  double degreeValue = simCore::Units::RADIANS.convertTo(simCore::Units::DEGREES, radianAngle);
+  if (wasNegative)
+    degreeValue = -degreeValue;
+
+  // extract minutes as fraction of degrees
+  double minuteValue = (degreeValue - floor(degreeValue)) * 60.0;
+
+  // truncate degrees
+  degreeValue = floor(degreeValue);
+
+  // same for seconds on minutes, with rounding
+  double secondValue = (minuteValue - floor(minuteValue)) * 60.0;
+  minuteValue = floor(minuteValue);
+
+  // Don't permit negative seconds
+  const double tolerance = 0.00000001; //< Value to avoid instance of -0
+  if (secondValue < tolerance)
+    secondValue = 0.0;
+
+  if ((secondValue + degreesTolerance_ > 60.0) || simCore::areEqual(secondValue + degreesTolerance_, 60.0))
+  {
+    secondValue = 0.0;
+    minuteValue += 1.0;
+    if (minuteValue >= 60.0)
+    {
+      minuteValue = 0.0;
+      degreeValue += 1.0;
+    }
+  }
+  else
+  {
+    double fraction = fmod(secondValue * scale_, 10.0);
+    if (fraction > 5.0)
+      secondValue += degreesTolerance_;
+    else if (simCore::areEqual(fraction, 5.0))
+      secondValue += degreesTolerance_ + 0.000001;
+  }
+
+  // degreeAngle was floor() above so just checking for 360 is OK
+  if (degreeValue == 360.0)
+    degreeValue = 0.0;
+  degreeValue = (wasNegative && printNegativeSign_) ? -degreeValue : degreeValue;
+
+  std::string angleString;
+
+  // SIM-14416: force leading -, as stringstream won't handle -0 as -0
+  if (wasNegative && printNegativeSign_ && degreeValue == -0.)
+    angleString = "-";
+  angleString += std::to_string(static_cast<int>(degreeValue));
+  angleString += degreeSymbolString_;
+  angleString += " ";
+
+  appendPadded_(angleString, static_cast<int>(minuteValue), 2);
+  angleString += minuteSymbolString_;
+  angleString += " ";
+
+  appendPadded_(angleString, static_cast<int>(floor(secondValue)), 2);
+  if (precision_ > 0)
+  {
+    double fractionalPart = secondValue - floor(secondValue);
+    std::string fracStr;
+    appendDouble_(fracStr, fractionalPart, precision_);
+    angleString += (fracStr.substr(1, precision_ + 1)); // skip '0' and take '.' + precision digits
+  }
+  angleString += (secondSymbolString_);
+
+  appendHemisphereDirection_(angleString, wasNegative);
+
+  return angleString;
+}
+
+std::string AngleFormatter::formatRadians_(double radianAngle) const
+{
+  const bool wasNegative = (radianAngle < 0.0);
+  if (!printNegativeSign_ && wasNegative)
+    radianAngle = -radianAngle;
+
+  std::string angleString;
+  appendDouble_(angleString, radianAngle, precision_);
+  appendHemisphereDirection_(angleString, wasNegative);
+
+  return angleString;
+}
+
+std::string AngleFormatter::formatMiscellaneous_(double radianAngle) const
+{
+  const bool wasNegative = (radianAngle < 0.0);
+  if (!printNegativeSign_ && wasNegative)
+    radianAngle = -radianAngle;
+
+  std::string angleString;
+  appendDouble_(angleString, simCore::Units::RADIANS.convertTo(formatToUnits(format_), radianAngle), precision_);
+  appendHemisphereDirection_(angleString, wasNegative);
+
+  return angleString;
+}
+
+std::string AngleFormatter::getHemisphereDirection_(bool wasNegative) const
+{
+  if (wasNegative)
+    return (!negativeDir_.empty() && !allNumerics_) ? negativeDir_ : "";
+
+  return (!positiveDir_.empty() && !allNumerics_) ? positiveDir_ : "";
+}
+
+bool AngleFormatter::getPrintNegativeSign_() const
+{
+  return (allNumerics_ || negativeDir_.empty());
+}
+
+void AngleFormatter::appendPadded_(std::string& str, int value, int width) const
+{
+  char buffer[16];
+  auto [ptr, ec] = std::to_chars(buffer, buffer + sizeof(buffer), value);
+  if (ec == std::errc())
+  {
+    if (int numChars = static_cast<int>(ptr - buffer); numChars < width)
+      str.append(width - numChars, '0');
+    str.append(buffer, ptr - buffer);
+  }
+}
+
+void AngleFormatter::appendDouble_(std::string& str, double value, size_t precision) const
+{
+  char buffer[64]; // Sufficient for most doubles
+  auto [ptr, ec] = std::to_chars(buffer, buffer + sizeof(buffer), value, std::chars_format::fixed, precision);
+  if (ec == std::errc())
+  {
+    str.append(buffer, ptr - buffer);
+  }
+}
+
+void AngleFormatter::appendHemisphereDirection_(std::string& str, bool wasNegative) const
+{
+  const std::string& hemiDir = getHemisphereDirection_(wasNegative);
+  if (!hemiDir.empty())
+  {
+    str += " ";
+    str += hemiDir;
+  }
+}
+
+void AngleFormatter::calculateScales_()
+{
+  scale_ = pow(10.0, precision_ + 1.0);
+  degreesTolerance_ = 5.0 / scale_;
+  radiansTolerance_ = degreesTolerance_ * DEG2RAD;
+}
+
+void AngleFormatter::setSymbols_()
+{
+  if (!allNumerics_)
+  {
+    degreeSymbolString_ = simCore::getDegreeSymbol(degSymbol_);
+    minuteSymbolString_ = "'";
+    secondSymbolString_ = "\"";
+  }
+  else
+  {
+    degreeSymbolString_.clear();
+    minuteSymbolString_.clear();
+    secondSymbolString_.clear();
+  }
 }
 
 } // namespace simCore
